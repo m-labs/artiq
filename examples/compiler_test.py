903cbfd84899a3af4d2210a82eea3529ff3aca1b
Sebastien Bourdeauducq <sb@m-labs.hk>
2014-06-16 21:39:33 +0200

    compiler_test: demo parameters and constant globals

--- examples/compiler_test.py
+++ examples/compiler_test.py
@@ -1,19 +1,22 @@
 from artiq.language.units import *
 from artiq.language.experiment import *
 
+my_range = range
+
 class CompilerTest(Experiment):
 	channels = "core a b A B"
 
 	@kernel
-	def run():
-		for i in range(3):
+	def run(self, n, t2):
+		t2 += 1*us
+		for i in my_range(n):
 			with parallel:
 				with sequential:
 					self.a.pulse(100*MHz, 20*us)
-					self.b.pulse(100*MHz, 10*us)
+					self.b.pulse(100*MHz, t2)
 				with sequential:
 					self.A.pulse(100*MHz, 10*us)
-					self.B.pulse(100*MHz, 10*us)
+					self.B.pulse(100*MHz, t2)
 
 if __name__ == "__main__":
 	from artiq.devices import core, core_dds
@@ -26,4 +29,4 @@ if __name__ == "__main__":
 		A=core_dds.DDS(coredev, 2, 2),
 		B=core_dds.DDS(coredev, 3, 3)
 	)
-	exp.run()
+	exp.run(3, 100*us)
