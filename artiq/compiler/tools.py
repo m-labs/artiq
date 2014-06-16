733a03b7ad5367b59d589280ef2eba5e19530476
Sebastien Bourdeauducq <sb@m-labs.hk>
2014-06-16 21:35:08 +0200

    cleanup

--- artiq/compiler/tools.py
+++ artiq/compiler/tools.py
@@ -4,4 +4,4 @@ def eval_ast(expr, symdict=dict()):
 	if not isinstance(expr, ast.Expression):
 		expr = ast.Expression(expr)
 	code = compile(expr, "<ast>", "eval")
-	return eval(code, symdict)#
+	return eval(code, symdict)
