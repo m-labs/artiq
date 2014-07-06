2aa63ba57d7d250eefb16f0b9850b7d42eb531f2
Sebastien Bourdeauducq <sb@m-labs.hk>
2014-07-06 18:29:02 +0200

    examples/coredev_test: print prime numbers

--- examples/coredev_test.py
+++ examples/coredev_test.py
@@ -1,4 +1,4 @@
-from artiq.language.experiment import Experiment, kernel
+from artiq.language.experiment import Experiment, kernel, syscall
 from artiq.devices import corecom_serial, runtime, core, gpio_core
 
 class CompilerTest(Experiment):
@@ -7,6 +7,18 @@ class CompilerTest(Experiment):
 	@kernel
 	def run(self):
 		self.led.set(1)
+		x = 1
+		while x < 100:
+			d = 2
+			prime = 1
+			while d*d <= x:
+				if x % d == 0:
+					prime = 0
+				d = d + 1
+			if prime == 1:
+				syscall("print_int", x)
+			x = x + 1
+		self.led.set(0)
 
 if __name__ == "__main__":
 	with corecom_serial.CoreCom() as com:
