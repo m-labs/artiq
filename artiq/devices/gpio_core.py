1885a8718700eaf83e7074e24127a32acc3c806c
Sebastien Bourdeauducq <sb@m-labs.hk>
2014-07-20 18:23:53 -0600

    devices/gpio_core: use MPO

--- artiq/devices/gpio_core.py
+++ artiq/devices/gpio_core.py
@@ -1,11 +1,7 @@
 from artiq.language.core import *
 
-class GPIOOut:
-	def __init__(self, core, channel=0):
-		self.core = core
-		self.channel = channel
-
-	kernel_attr_ro = "channel"
+class GPIOOut(MPO):
+	parameters = "channel"
 
 	@kernel
 	def set(self, level):
