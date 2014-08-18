62b872ad944280565eedd0255dfe729435899593
Sebastien Bourdeauducq <sb@m-labs.hk>
2014-08-18 23:53:10 +0800

    devices/dds_core: use new compiler features to compute tuning word trivially

--- artiq/devices/dds_core.py
+++ artiq/devices/dds_core.py
@@ -13,8 +13,7 @@ class DDS(AutoContext):
 	def pulse(self, frequency, duration):
 		if self._previous_frequency != frequency:
 			syscall("rtio_sync", self.rtio_channel) # wait until output is off
-			syscall("dds_program", self.reg_channel,
-				(frequency << 2)//(self.dds_sysclk >> 15) << 15)
+			syscall("dds_program", self.reg_channel, int(2**32*frequency/self.dds_sysclk))
 			self._previous_frequency = frequency
 		syscall("rtio_set", now(), self.rtio_channel, 1)
 		delay(duration)
