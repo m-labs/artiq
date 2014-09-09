e6530075d9ff361558c08b77d279d090674c1145
Sebastien Bourdeauducq <sb@m-labs.hk>
2014-09-09 22:00:51 +0800

    devices/dds_core: add on/off methods

--- artiq/devices/dds_core.py
+++ artiq/devices/dds_core.py
@@ -11,12 +11,25 @@ class DDS(AutoContext):
     kernel_attr = "_previous_frequency"
 
     @kernel
-    def pulse(self, frequency, duration):
+    def _set_frequency(self, frequency):
         if self._previous_frequency != frequency:
             syscall("rtio_sync", self.rtio_channel)  # wait until output is off
             syscall("dds_program", self.reg_channel,
                     int(2**32*frequency/self.dds_sysclk))
             self._previous_frequency = frequency
+
+    @kernel
+    def pulse(self, frequency, duration):
+        self._set_frequency(frequency)
         syscall("rtio_set", now(), self.rtio_channel, 1)
         delay(duration)
+        syscall("rtio_set", now(), self.rtio_channel, 0)
+
+    @kernel
+    def on(self, frequency):
+        self._set_frequency(frequency)
+        syscall("rtio_set", now(), self.rtio_channel, 1)
+
+    @kernel
+    def off(self):
         syscall("rtio_set", now(), self.rtio_channel, 0)
