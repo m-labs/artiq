e2ca571c8942213d4ef854d1828d8c1714fadfdf
Sebastien Bourdeauducq <sb@m-labs.hk>
2014-09-09 17:13:26 +0800

    transforms/inline: let array calls through

--- artiq/transforms/inline.py
+++ artiq/transforms/inline.py
@@ -32,7 +32,7 @@ class _ReferenceManager:
         # reserved names
         for kg in core_language.kernel_globals:
             self.use_count[kg] = 1
-        for name in ("int", "round", "int64", "round64",
+        for name in ("int", "round", "int64", "round64", "array",
                      "range", "Fraction", "Quantity",
                      "s_unit", "Hz_unit", "microcycle_unit"):
             self.use_count[name] = 1
@@ -113,7 +113,8 @@ class _ReferenceManager:
 _embeddable_calls = {
     core_language.delay, core_language.at, core_language.now,
     core_language.syscall,
-    range, int, round, core_language.int64, core_language.round64,
+    range, int, round,
+    core_language.int64, core_language.round64, core_language.array,
     Fraction, units.Quantity
 }
 
