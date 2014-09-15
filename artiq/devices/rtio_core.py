140b4eb594dd3a7103e5d28ff81ade324a0bf8f8
Sebastien Bourdeauducq <sb@m-labs.hk>
2014-09-15 22:48:22 +0800

    devices/rtio_core: set OE, implement gate/count

--- artiq/devices/rtio_core.py
+++ artiq/devices/rtio_core.py
@@ -1,7 +1,7 @@
 from artiq.language.core import *
 
 
-class RTIOOut(AutoContext):
+class _RTIOBase(AutoContext):
     parameters = "channel"
 
     def build(self):
@@ -9,6 +9,10 @@ class RTIOOut(AutoContext):
         self.previous_value = 0
 
     kernel_attr = "previous_timestamp previous_value"
+
+    @kernel
+    def _set_oe(self, oe):
+        syscall("rtio_oe", self.channel, oe)
 
     @kernel
     def _set_value(self, value):
@@ -19,6 +23,12 @@ class RTIOOut(AutoContext):
                 syscall("rtio_set", now(), self.channel, value)
             self.previous_timestamp = now()
             self.previous_value = value
+
+
+class RTIOOut(_RTIOBase):
+    def build(self):
+        _RTIOBase.build(self)
+        self._set_oe(1)
 
     @kernel
     def sync(self):
@@ -39,13 +49,32 @@ class RTIOOut(AutoContext):
         self.off()
 
 
-class RTIOCounter(AutoContext):
-    parameters = "channel"
+class RTIOCounter(_RTIOBase):
+    def build(self):
+        _RTIOBase.build(self)
+        self._set_oe(0)
 
     @kernel
     def count_rising(self, duration):
-        pass
+        self._set_value(1)
+        delay(duration)
+        self._set_value(0)
+
+    @kernel
+    def count_falling(self, duration):
+        self._set_value(2)
+        delay(duration)
+        self._set_value(0)
+
+    @kernel
+    def count_both_edges(self, duration):
+        self._set_value(3)
+        delay(duration)
+        self._set_value(0)
 
     @kernel
     def sync(self):
-        return 42
+        count = 0
+        while syscall("rtio_get", self.channel) >= 0:
+            count += 1
+        return count
