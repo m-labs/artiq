44ec3eae3daef05d3ae8970ee60c8e40907798f4
Sebastien Bourdeauducq <sb@m-labs.hk>
2014-11-28 10:21:43 +0800

    soc/target: use minicon by default

--- soc/targets/artiq.py
+++ soc/targets/artiq.py
@@ -45,8 +45,11 @@ class ARTIQMiniSoC(BaseSoC):
     }
     csr_map.update(BaseSoC.csr_map)
 
-    def __init__(self, platform, cpu_type="or1k", with_test_gen=False, **kwargs):
-        BaseSoC.__init__(self, platform, cpu_type=cpu_type, **kwargs)
+    def __init__(self, platform, cpu_type="or1k", ramcon_type="minicon",
+                 with_test_gen=False, **kwargs):
+        BaseSoC.__init__(self, platform,
+                         cpu_type=cpu_type, ramcon_type=ramcon_type,
+                         **kwargs)
         platform.add_extension(_tester_io)
 
         self.submodules.leds = gpio.GPIOOut(Cat(
