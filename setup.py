0dc4eb02aed6770af2fe88219e90996c8344eabe
Sebastien Bourdeauducq <sb@m-labs.hk>
2014-12-10 12:13:10 +0800

    setup: install frontend tools, remove nosetest dependency, minor fixes

--- setup.py
+++ setup.py
@@ -1,26 +1,29 @@
-#!/usr/bin/python
+#!/usr/bin/env python3
 # -*- coding: utf8 -*-
 
 from setuptools import setup, find_packages
 from glob import glob
+import os
+
 
 setup(
-        name = "artiq",
-        version = "0.0+dev",
-        author = u"Sébastien Bourdeauducq & NIST Ion Storage Group",
-        author_email = "sb@m-labs.hk",
-        url = "http://github.com/m-labs/artiq/",
-        description = "Experiment Control",
-        long_description = open("README.rst").read(),
-        license = "BSD",
-        install_requires = [
-            "numpy", "scipy", "sphinx", "numpydoc", "nose",
-        ],
-        extras_require = {},
-        dependency_links = [],
-        packages = find_packages(),
-        namespace_packages = [],
-        test_suite = "nose.collector",
-        include_package_data = True,
-        ext_modules=[],
-        )
+    name = "artiq",
+    version = "0.0+dev",
+    author = "M-Labs / NIST Ion Storage Group",
+    author_email = "sb@m-labs.hk",
+    url = "http://m-labs.hk/artiq",
+    description = "A control system for trapped-ion experiments",
+    long_description = open("README.rst").read(),
+    license = "BSD",
+    install_requires = [
+        "sphinx", "numpy", "scipy"
+    ],
+    extras_require = {},
+    dependency_links = [],
+    packages = find_packages(),
+    namespace_packages = [],
+    test_suite = "artiq.test",
+    include_package_data = True,
+    ext_modules=[],
+    scripts=glob(os.path.join("frontend", "*.py"))
+)
