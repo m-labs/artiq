32b79207b6e91599df6987d0b90d59fb49b94b34
Sebastien Bourdeauducq <sb@m-labs.hk>
2014-12-30 18:53:27 +0800

    setup.py: fix data file installation

--- setup.py
+++ setup.py
@@ -6,23 +6,25 @@ import os
 
 
 setup(
-    name = "artiq",
-    version = "0.0+dev",
-    author = "M-Labs / NIST Ion Storage Group",
-    author_email = "sb@m-labs.hk",
-    url = "http://m-labs.hk/artiq",
-    description = "A control system for trapped-ion experiments",
-    long_description = open("README.rst").read(),
-    license = "BSD",
-    install_requires = [
+    name="artiq",
+    version="0.0+dev",
+    author="M-Labs / NIST Ion Storage Group",
+    author_email="sb@m-labs.hk",
+    url="http://m-labs.hk/artiq",
+    description="A control system for trapped-ion experiments",
+    long_description=open("README.rst").read(),
+    license="BSD",
+    install_requires=[
         "sphinx", "pyserial", "numpy", "scipy", "prettytable"
     ],
-    extras_require = {},
-    dependency_links = [],
-    packages = find_packages(),
-    namespace_packages = [],
-    test_suite = "artiq.test",
-    include_package_data = True,
+    extras_require={},
+    dependency_links=[],
+    packages=find_packages(),
+    namespace_packages=[],
+    test_suite="artiq.test",
+    data_files=[
+        (os.path.join("artiq", "gui"),
+         [os.path.join("artiq", "gui", "icon.png")])],
     ext_modules=[],
     scripts=glob(os.path.join("frontend", "*.py"))
 )
