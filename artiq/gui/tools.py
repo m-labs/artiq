2a60c7475ea6df9a955be056a15b5f8e737d50ed
Sebastien Bourdeauducq <sb@m-labs.hk>
2014-12-30 19:19:26 +0800

    gui: shorten name

--- artiq/gui/tools.py
+++ artiq/gui/tools.py
@@ -8,6 +8,6 @@ data_dir = os.path.abspath(os.path.dirname(__file__))
 class Window(Gtk.Window):
     def __init__(self, *args, **kwargs):
         Gtk.Window.__init__(self, *args, **kwargs)
-        self.set_wmclass("ARTIQ GUI", "ARTIQ GUI")
+        self.set_wmclass("ARTIQ", "ARTIQ")
         self.set_icon_from_file(os.path.join(data_dir, "icon.png"))
         self.set_border_width(6)
