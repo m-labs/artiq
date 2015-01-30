9bfc2070d4f51ffe972aa8270753772c9d23ebac
Sebastien Bourdeauducq <sb@m-labs.hk>
2015-01-30 19:07:51 +0800

    examples/rtio_skew: show result in GUI

--- examples/rtio_skew.py
+++ examples/rtio_skew.py
@@ -1,8 +1,8 @@
 from artiq import *
 
 
-def print_failed():
-    print("Pulse was not received back")
+class PulseNotReceived(Exception):
+    pass
 
 
 class RTIOSkew(AutoDB):
@@ -10,6 +10,12 @@ class RTIOSkew(AutoDB):
         pmt0 = Device()
         ttl0 = Device()
         io_skew = Result()
+
+    @staticmethod
+    def realtime_results():
+        return {
+            "io_skew": "raw"
+        }
 
     @kernel
     def run(self):
@@ -21,6 +27,5 @@ class RTIOSkew(AutoDB):
                 self.ttl0.pulse(5*us)
         in_t = self.pmt0.timestamp()
         if in_t < 0*s:
-            print_failed()
-        else:
-            self.io_skew = out_t - in_t
+            raise PulseNotReceived
+        self.io_skew = out_t - in_t
