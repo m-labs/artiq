e4251c7f414b0cebaf2912b04d3cc46b3332796c
Sebastien Bourdeauducq <sb@m-labs.hk>
2015-04-22 15:01:32 +0800

    runtime: get lwip to run

--- soc/runtime/main.c
+++ soc/runtime/main.c
@@ -6,9 +6,65 @@
 #include <system.h>
 #include <time.h>
 #include <generated/csr.h>
+#include <hw/flags.h>
+
+#ifdef CSR_ETHMAC_BASE
+#include <netif/etharp.h>
+#include <netif/liteethif.h>
+#include <lwip/init.h>
+#include <lwip/memp.h>
+#include <lwip/ip4_addr.h>
+#include <lwip/ip4.h>
+#include <lwip/netif.h>
+#include <lwip/sys.h>
+#include <lwip/tcp.h>
+#include <lwip/timers.h>
+#endif
 
 #include "test_mode.h"
 #include "session.h"
+
+#ifdef CSR_ETHMAC_BASE
+unsigned char macadr[6] = {0x10, 0xe2, 0xd5, 0x00, 0x00, 0x00};
+
+u32_t clock_ms;
+
+static void clock_init(void)
+{
+    timer0_en_write(0);
+    timer0_load_write(0xffffffff);
+    timer0_reload_write(0xffffffff);
+    timer0_en_write(1);
+    clock_ms = 0;
+}
+
+u32_t sys_now(void)
+{
+    unsigned int freq;
+    unsigned int prescaler;
+
+    freq = identifier_frequency_read();
+    prescaler = freq/1000; /* sys_now expect time in ms */
+    timer0_update_value_write(1);
+    clock_ms += (0xffffffff - timer0_value_read())/prescaler;
+    /* Reset timer to avoid rollover, this will increase clock_ms
+       drift but we don't need precision */
+    timer0_en_write(0);
+    timer0_en_write(1);
+    return clock_ms;
+}
+
+static struct netif netif;
+
+static void lwip_service(void)
+{
+    sys_check_timeouts();
+    if(ethmac_sram_writer_ev_pending_read() & ETHMAC_EV_SRAM_WRITER) {
+        liteeth_input(&netif);
+        ethmac_sram_writer_ev_pending_write(ETHMAC_EV_SRAM_WRITER);
+    }
+}
+#endif
 
 void comm_service(void)
 {
@@ -17,6 +73,10 @@ void comm_service(void)
     static char rxdata;
     static int rxpending;
     int r, i;
+
+#ifdef CSR_ETHMAC_BASE
+        lwip_service();
+#endif
 
     if(!rxpending && uart_read_nonblock()) {
         rxdata = uart_read();
@@ -38,6 +98,26 @@ void comm_service(void)
 
 static void regular_main(void)
 {
+#ifdef CSR_ETHMAC_BASE
+    struct ip4_addr local_ip;
+    struct ip4_addr netmask;
+    struct ip4_addr gateway_ip;
+
+    time_init();
+    clock_init();
+
+    IP4_ADDR(&local_ip, 192, 168, 0, 42);
+    IP4_ADDR(&netmask, 255, 255, 255, 0);
+    IP4_ADDR(&gateway_ip, 192, 168, 0, 1);
+
+    lwip_init();
+
+    netif_add(&netif, &local_ip, &netmask, &gateway_ip, 0, liteeth_init, ethernet_input);
+    netif_set_default(&netif);
+    netif_set_up(&netif);
+    netif_set_link_up(&netif);
+#endif
+
     session_start();
     while(1)
         comm_service();
