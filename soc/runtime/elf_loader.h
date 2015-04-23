d99976dc37973ff7df3ee5ab9857ccd8401b77ab
Sebastien Bourdeauducq <sb@m-labs.hk>
2015-04-23 19:06:23 +0800

    runtime/elf_loader: add alignment comment

--- soc/runtime/elf_loader.h
+++ soc/runtime/elf_loader.h
@@ -10,6 +10,7 @@ typedef void * (*symbol_resolver)(const char *);
 typedef int (*symbol_callback)(const char *, void *);
 
 void *find_symbol(const struct symbol *symbols, const char *name);
+/* elf_data must be aligned on a 32-bit boundary */
 int load_elf(symbol_resolver resolver, symbol_callback callback, void *elf_data, int elf_length, void *dest, int dest_length);
 
 #endif /* __ELF_LOADER_H */
