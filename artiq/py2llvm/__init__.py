9953302cb64dc56f618568bd327e4c397517f747
whitequark <whitequark@whitequark.org>
2015-06-11 02:58:29 +0300

    Move old py2llvm code to artiq/py2llvm_old.

--- artiq/py2llvm/__init__.py
+++
@@ -1,6 +0,0 @@
-from artiq.py2llvm.module import Module
-
-def get_runtime_binary(env, func_def):
-    module = Module(env)
-    module.compile_function(func_def, dict())
-    return module.emit_object()
