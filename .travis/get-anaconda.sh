b820b7a3e1b7e59bb3eedca6386ea6b4ebdc2714
Robert Jordens <jordens@gmail.com>
2015-06-25 12:56:50 -0600

    Revert "travis: use latest miniconda"
    Seems to segfault on travis.
    
    This reverts commit 51eb8350302af7310f8ed8fe35d41f9239a191b5.

--- .travis/get-anaconda.sh
+++ .travis/get-anaconda.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 export PATH=$HOME/miniconda/bin:$PATH
-wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
+wget http://repo.continuum.io/miniconda/Miniconda3-3.7.3-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 hash -r
 conda config --set always_yes yes --set changeps1 no
