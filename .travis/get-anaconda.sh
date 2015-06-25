1c7ab97b9199ab26bac854ff7f69f4d56cca88fa
Robert Jordens <jordens@gmail.com>
2015-06-25 12:40:04 -0600

    travis: cleanup unused/redundant env vars

--- .travis/get-anaconda.sh
+++ .travis/get-anaconda.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
+export PATH=$HOME/miniconda/bin:$PATH
 wget http://repo.continuum.io/miniconda/Miniconda3-3.7.3-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
 hash -r
