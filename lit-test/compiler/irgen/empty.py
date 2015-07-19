f5d9e11b38467a66608c2e16526416fc831d7e8e
whitequark <whitequark@whitequark.org>
2015-07-19 11:30:53 +0300

    Remove irgen tests.
    These are too hard to write and will be replaced by integration
    tests of ARTIQ IR generator + LLVM IR generator once the latter
    gets implemented.

--- lit-test/compiler/irgen/empty.py
+++
@@ -1,7 +0,0 @@
-# RUN: %python -m artiq.compiler.testbench.irgen %s >%t
-# RUN: OutputCheck %s --file-to-check=%t
-
-# CHECK-L: NoneType input.__modinit__() {
-# CHECK-L: 1:
-# CHECK-L:   return NoneType None
-# CHECK-L: }
--- lit-test/compiler/irgen/eval.py
+++
@@ -1,9 +0,0 @@
-# RUN: %python -m artiq.compiler.testbench.irgen %s >%t
-# RUN: OutputCheck %s --file-to-check=%t
-
-2 + 2
-# CHECK-L: NoneType input.__modinit__() {
-# CHECK-L: 1:
-# CHECK-L:   %2 = int(width=32) eval `2 + 2`
-# CHECK-L:   return NoneType None
-# CHECK-L: }
