64d2604aa8416fea585a525400177a82b07b4bd6
whitequark <whitequark@whitequark.org>
2015-07-21 14:12:27 +0300

    Tolerate assertion failures in tests when looking for diagnostics.

--- artiq/compiler/testbench/module.py
+++ artiq/compiler/testbench/module.py
@@ -5,11 +5,13 @@ from .. import Module
 def main():
     if len(sys.argv) > 1 and sys.argv[1] == "+diag":
         del sys.argv[1]
+        diag = True
         def process_diagnostic(diag):
             print("\n".join(diag.render(only_line=True)))
             if diag.level == "fatal":
                 exit()
     else:
+        diag = False
         def process_diagnostic(diag):
             print("\n".join(diag.render()))
             if diag.level in ("fatal", "error"):
@@ -18,8 +20,11 @@ def main():
     engine = diagnostic.Engine()
     engine.process = process_diagnostic
 
-    mod = Module.from_string("".join(fileinput.input()).expandtabs(), engine=engine)
-    print(repr(mod))
+    try:
+        mod = Module.from_string("".join(fileinput.input()).expandtabs(), engine=engine)
+        print(repr(mod))
+    except:
+        if not diag: raise
 
 if __name__ == "__main__":
     main()
