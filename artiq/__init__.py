6b98f867de148c538fe82af55184803dc845dce8
Sebastien Bourdeauducq <sb@m-labs.hk>
2015-07-29 23:32:33 +0800

    import DDS phase modes at the top level

--- artiq/__init__.py
+++ artiq/__init__.py
@@ -1,5 +1,9 @@
 from artiq import language
 from artiq.language import *
+from artiq.coredevice.dds import (PHASE_MODE_CONTINUOUS, PHASE_MODE_ABSOLUTE,
+                                  PHASE_MODE_TRACKING)
 
 __all__ = []
 __all__.extend(language.__all__)
+__all__ += ["PHASE_MODE_CONTINUOUS", "PHASE_MODE_ABSOLUTE",
+            "PHASE_MODE_TRACKING"]
