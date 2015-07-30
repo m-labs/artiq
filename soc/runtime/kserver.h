697b78ddf2fbdf02b300f477c447f0547b8dce13
whitequark <whitequark@whitequark.org>
2015-07-30 13:45:57 +0300

    Rename {kserver → net_server}.{c,h}.

--- soc/runtime/kserver.h
+++
@@ -1,7 +0,0 @@
-#ifndef __KSERVER_H
-#define __KSERVER_H
-
-void kserver_init(void);
-void kserver_service(void);
-
-#endif /* __KSERVER_H */
---
+++ soc/runtime/net_server.h
@@ -0,0 +1,7 @@
+#ifndef __NET_SERVER_H
+#define __NET_SERVER_H
+
+void net_server_init(void);
+void net_server_service(void);
+
+#endif /* __NET_SERVER_H */
