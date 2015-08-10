c57ce6d75006c211d7be4331a0a89103c6c268eb
Yann Sionneau <ys@m-labs.hk>
2015-08-10 12:23:05 +0200

    conda: llvmdev should be built in Release mode with assertions enabled

--- conda/llvmdev-or1k/build.sh
+++ conda/llvmdev-or1k/build.sh
@@ -5,6 +5,6 @@ git clone https://github.com/openrisc/clang-or1k clang
 cd ..
 mkdir build
 cd build
-cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DLLVM_TARGETS_TO_BUILD="OR1K;X86" -DCMAKE_BUILD_TYPE=Debug
+cmake .. -DCMAKE_INSTALL_PREFIX=$PREFIX -DLLVM_TARGETS_TO_BUILD="OR1K;X86" -DCMAKE_BUILD_TYPE=Rel -DLLVM_ENABLE_ASSERTIONS=ON
 make -j2
 make install
