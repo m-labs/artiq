eaa317e08f635206f592f0c4984a98d689394825
Sebastien Bourdeauducq <sb@m-labs.hk>
2015-10-04 23:16:40 +0800

    travis: switch to py3.5 sooner

--- .travis/get-anaconda.sh
+++ .travis/get-anaconda.sh
@@ -4,10 +4,10 @@
 export PATH=$HOME/miniconda/bin:$PATH
 wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O miniconda.sh
 bash miniconda.sh -b -p $HOME/miniconda
+conda create -q -n py35 python=$TRAVIS_PYTHON_VERSION
 hash -r
 conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda info -a
 conda install conda-build jinja2
-conda create -q -n py35 python=$TRAVIS_PYTHON_VERSION
 conda config --add channels https://conda.anaconda.org/m-labs/channel/dev
