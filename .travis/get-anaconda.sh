342e72bed66ee3ea4183ee24b7f8923496b0b6aa
Sebastien Bourdeauducq <sb@m-labs.hk>
2015-10-05 00:09:36 +0800

    more travis fixing attempts

--- .travis/get-anaconda.sh
+++ .travis/get-anaconda.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/sh
 # Copyright (C) 2014, 2015 Robert Jordens <jordens@gmail.com>
 
 export PATH=$HOME/miniconda/bin:$PATH
@@ -6,9 +6,8 @@ wget http://repo.continuum.io/miniconda/Miniconda3-latest-Linux-x86_64.sh -O min
 bash miniconda.sh -b -p $HOME/miniconda
 hash -r
 conda config --set always_yes yes --set changeps1 no
-conda create -q -n py35 python=$TRAVIS_PYTHON_VERSION
-source $HOME/miniconda/bin/activate py35
 conda update -q conda
 conda info -a
 conda install conda-build jinja2
+conda create -q -n py35 python=$TRAVIS_PYTHON_VERSION
 conda config --add channels https://conda.anaconda.org/m-labs/channel/dev
