3cf53667c8e2c5b4fe5a87ae7a3624aba0139590
Sebastien Bourdeauducq <sb@m-labs.hk>
2015-10-06 13:50:29 +0800

    examples: add RunForever experiment to demonstrate graceful termination

---
+++ examples/master/repository/run_forever.py
@@ -0,0 +1,18 @@
+from itertools import count
+from time import sleep
+
+from artiq import *
+
+
+class RunForever(EnvExperiment):
+    def build(self):
+        self.setattr_device("scheduler")
+
+    def run(self):
+        try:
+            for i in count():
+                self.scheduler.pause()
+                sleep(1)
+                print("ping", i)
+        except TerminationRequested:
+            print("Terminated gracefully")
