b6c8c9f480938886d825b098e1ce81b84c9d8c12
whitequark <whitequark@whitequark.org>
2015-10-09 01:37:28 +0300

    transforms.artiq_ir_generator: add tests for devirtualization.

---
+++ lit-test/test/devirtualization/function.py
@@ -0,0 +1,22 @@
+# RUN: env ARTIQ_DUMP_IR=1 %python -m artiq.compiler.testbench.embedding +compile %s 2>%t
+# RUN: OutputCheck %s --file-to-check=%t
+
+from artiq.language.core import *
+from artiq.language.types import *
+
+# CHECK-L: call ()->NoneType %local.testbench.entrypoint ; calls testbench.entrypoint
+
+@kernel
+def baz():
+    pass
+
+class foo:
+    @kernel
+    def bar(self):
+        # CHECK-L: call ()->NoneType %local.testbench.baz ; calls testbench.baz
+        baz()
+x = foo()
+
+@kernel
+def entrypoint():
+    x.bar()
