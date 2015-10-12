3923dd83c2b260766c2b4f9e0b3274b1f5639075
Sebastien Bourdeauducq <sb@m-labs.hk>
2015-10-12 17:20:01 +0800

    protocols/file_db: remove unneeded hooks feature

--- artiq/protocols/file_db.py
+++ artiq/protocols/file_db.py
@@ -1,5 +1,3 @@
-from time import time
-
 from artiq.protocols import pyon
 from artiq.protocols.sync_struct import Notifier
 
@@ -8,7 +6,6 @@ class FlatFileDB:
     def __init__(self, filename):
         self.filename = filename
         self.data = Notifier(pyon.load_file(self.filename))
-        self.hooks = []
 
     def save(self):
         pyon.store_file(self.filename, self.data.read)
@@ -19,13 +16,7 @@ class FlatFileDB:
     def set(self, key, value):
         self.data[key] = value
         self.save()
-        timestamp = time()
-        for hook in self.hooks:
-            hook.set(timestamp, key, value)
 
     def delete(self, key):
         del self.data[key]
         self.save()
-        timestamp = time()
-        for hook in self.hooks:
-            hook.delete(timestamp, key)
