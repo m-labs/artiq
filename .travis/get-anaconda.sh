5d89acbe82cd9d08123c4b3cbd4b42c217f07a5e
whitequark <whitequark@whitequark.org>
2015-11-24 21:43:00 +0800

    conda: switch back to default miniconda python, which is now 3.5.

--- .travis/get-anaconda.sh
+++ .travis/get-anaconda.sh
@@ -11,6 +11,5 @@ conda config --set always_yes yes --set changeps1 no
 conda update -q conda
 conda info -a
 conda install conda-build jinja2
-conda create -q -n py35 python=$TRAVIS_PYTHON_VERSION
 conda config --add channels https://conda.anaconda.org/m-labs/channel/main
 conda config --add channels https://conda.anaconda.org/m-labs/channel/dev
