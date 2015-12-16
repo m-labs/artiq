142b9b059bf50138f22e654553b802bb1ddc3110
whitequark <whitequark@whitequark.org>
2015-12-16 19:39:07 +0800

    lit-test: add tests for control flow in parallel blocks.

---
+++ lit-test/test/interleaving/control_flow.py
@@ -0,0 +1,17 @@
+# RUN: %python -m artiq.compiler.testbench.jit %s >%t
+# RUN: OutputCheck %s --file-to-check=%t
+
+def f():
+    with parallel:
+        if True:
+            print(1)
+        else:
+            print(2)
+        while False:
+            print(3)
+        delay_mu(1)
+    print(4)
+
+# CHECK-L: 1
+# CHECK-L: 4
+f()
