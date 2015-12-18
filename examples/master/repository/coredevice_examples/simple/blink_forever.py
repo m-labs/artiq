40e10caf0af2adf2239c088fdf6b81be12876417
Sebastien Bourdeauducq <sb@m-labs.hk>
2015-12-18 15:09:56 +0800

    examples/blink_forever: revert incorrectly committed changes

--- examples/master/repository/coredevice_examples/simple/blink_forever.py
+++ examples/master/repository/coredevice_examples/simple/blink_forever.py
@@ -4,17 +4,10 @@ from artiq import *
 class BlinkForever(EnvExperiment):
     def build(self):
         self.setattr_device("core")
-        self.setattr_device("ttl0")
-        self.setattr_device("ttl1")
-
-    def hello(self, i):
-        print("Hello world", i)
+        self.setattr_device("led")
 
     @kernel
     def run(self):
-        for i in range(80000):
-            self.ttl0.pulse(40*us)
-            self.ttl1.pulse(40*us)
-            delay(40*us)
-        for i in range(7):
-            self.hello(i)
+        while True:
+            self.led.pulse(100*ms)
+            delay(100*ms)
