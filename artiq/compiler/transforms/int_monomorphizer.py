54aeb5030d46a6e294d2ece4a36b515e0a87266d
whitequark <whitequark@whitequark.org>
2015-12-20 02:36:30 +0800

    transforms.int_monomorphizer: also monomorphize round() calls (#198).

--- artiq/compiler/transforms/int_monomorphizer.py
+++ artiq/compiler/transforms/int_monomorphizer.py
@@ -26,3 +26,10 @@ class IntMonomorphizer(algorithm.Visitor):
                     return
 
                 node.type["width"].unify(types.TValue(width))
+
+    def visit_CallT(self, node):
+        if types.is_builtin(node.func.type, "int") or \
+                types.is_builtin(node.func.type, "round"):
+            typ = node.type.find()
+            if types.is_var(typ["width"]):
+                typ["width"].unify(types.TValue(32))
