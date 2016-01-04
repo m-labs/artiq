d64d962c3e9cd1fb85584b81c275c484cd571b0c
Robert Jordens <jordens@gmail.com>
2016-01-04 16:52:53 -0700

    remove redundant udev rules, update entry points

--- conda/artiq/build.sh
+++ conda/artiq/build.sh
@@ -3,13 +3,3 @@
 ARTIQ_PREFIX=$PREFIX/lib/python3.5/site-packages/artiq
 
 $PYTHON setup.py install --single-version-externally-managed --record=record.txt
-
-# install scripts
-
-cp artiq/frontend/artiq_flash.sh $PREFIX/bin
-
-# install udev rules
-
-mkdir -p $ARTIQ_PREFIX/misc
-cp misc/99-papilio.rules $ARTIQ_PREFIX/misc/
-cp misc/99-kc705.rules $ARTIQ_PREFIX/misc/
