e92d52314bd663122a5a52f4da3346bfe3ef2d1a
Robert Jordens <jordens@gmail.com>
2016-01-28 18:02:51 -0700

    flash storage: move to flash + 0x70000
    This requires recompiling and flashing the runtime and the entire
    storage area file system (ip address and idle kernel).
    
    * with ppp the runtime is larger than 0x40000 now
    * 0x60000 for the runtime should give enough space for a while,
      even including a fatter scheduler or runtime components
      in other languages
    * for both pipistrello and kc705 this leaves the unused space
      starting at a nice round number
    * the relevant flashes nowadays are 0x1000000 large

--- artiq/frontend/artiq_flash.py
+++ artiq/frontend/artiq_flash.py
@@ -58,7 +58,7 @@ def main():
             "bitstream": 0x000000,
             "bios": 0xaf0000,
             "runtime": 0xb00000,
-            "storage": 0xb40000,
+            "storage": 0xb70000,
         },
         "pipistrello": {
             "chip": "xc6slx45",
@@ -66,7 +66,7 @@ def main():
             "bitstream": 0x000000,
             "bios": 0x170000,
             "runtime": 0x180000,
-            "storage": 0x1c0000,
+            "storage": 0x1f0000,
         },
     }[opts.target]
 
