950eaef08cf2141d8565965d8cbf2f4cc3710e7d
whitequark <whitequark@whitequark.org>
2016-02-24 13:48:05 +0000

    coredevice: re-export more exceptions.

--- artiq/coredevice/__init__.py
+++ artiq/coredevice/__init__.py
@@ -1,10 +1,12 @@
 from artiq.coredevice import exceptions, dds
 from artiq.coredevice.exceptions import (RTIOUnderflow, RTIOSequenceError,
-                                         RTIOCollisionError)
+                                         RTIOCollisionError, RTIOOverflow,
+                                         DDSBatchError, CacheError)
 from artiq.coredevice.dds import (PHASE_MODE_CONTINUOUS, PHASE_MODE_ABSOLUTE,
                                   PHASE_MODE_TRACKING)
 
 __all__ = []
-__all__ += ["RTIOUnderflow", "RTIOSequenceError", "RTIOCollisionError"]
+__all__ += ["RTIOUnderflow", "RTIOSequenceError", "RTIOCollisionError",
+            "RTIOOverflow", "DDSBatchError", "CacheError"]
 __all__ += ["PHASE_MODE_CONTINUOUS", "PHASE_MODE_ABSOLUTE",
             "PHASE_MODE_TRACKING"]
