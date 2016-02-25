18274a09a19694cede3bf3eb63440d1f5158490f
whitequark <whitequark@whitequark.org>
2016-02-25 02:05:44 +0000

    Commit missing parts of 4426730.

--- artiq/test/lit/devirtualization/method.py
+++ artiq/test/lit/devirtualization/method.py
@@ -1,5 +1,5 @@
-# RUN: env ARTIQ_DUMP_IR=1 %python -m artiq.compiler.testbench.embedding +compile %s 2>%t
-# RUN: OutputCheck %s --file-to-check=%t
+# RUN: env ARTIQ_DUMP_IR=%t %python -m artiq.compiler.testbench.embedding +compile %s 2>%t
+# RUN: OutputCheck %s --file-to-check=%t.txt
 
 from artiq.language.core import *
 from artiq.language.types import *
