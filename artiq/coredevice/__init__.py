d0af58d122c1a2b84ed4b4a1957d52c06093ef8b
Sebastien Bourdeauducq <sb@m-labs.hk>
2016-03-19 12:11:47 +0800

    coredevice: only import common RTIO exceptions

--- artiq/coredevice/__init__.py
+++ artiq/coredevice/__init__.py
@@ -1,12 +1,11 @@
 from artiq.coredevice import exceptions, dds, spi
 from artiq.coredevice.exceptions import (RTIOUnderflow, RTIOSequenceError,
-                                         RTIOCollision, RTIOOverflow, RTIOBusy,
-                                         DDSBatchError, CacheError)
+                                         RTIOCollision, RTIOOverflow, RTIOBusy)
 from artiq.coredevice.dds import (PHASE_MODE_CONTINUOUS, PHASE_MODE_ABSOLUTE,
                                   PHASE_MODE_TRACKING)
 
 __all__ = []
 __all__ += ["RTIOUnderflow", "RTIOSequenceError", "RTIOCollision",
-            "RTIOOverflow", "RTIOBusy", "DDSBatchError", "CacheError"]
+            "RTIOOverflow", "RTIOBusy"]
 __all__ += ["PHASE_MODE_CONTINUOUS", "PHASE_MODE_ABSOLUTE",
             "PHASE_MODE_TRACKING"]
