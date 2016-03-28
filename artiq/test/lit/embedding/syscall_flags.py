f31249ad1cfec46bbd9f85c5eee67c73f54d5672
whitequark <whitequark@whitequark.org>
2016-03-28 20:28:08 +0000

    Commit missing parts of 6f5332f8.

---
+++ artiq/test/lit/embedding/syscall_flags.py
@@ -0,0 +1,18 @@
+# RUN: env ARTIQ_DUMP_LLVM=%t %python -m artiq.compiler.testbench.embedding +compile %s
+# RUN: OutputCheck %s --file-to-check=%t.ll
+
+from artiq.language.core import *
+from artiq.language.types import *
+
+# CHECK: call void @foo\(\), !dbg !\d+, !tbaa !\d+
+
+# CHECK-L: ; Function Attrs: nounwind
+# CHECK-NEXT-L: declare void @foo()
+
+@syscall("foo", flags={"nounwind", "nowrite"})
+def foo() -> TNone:
+    pass
+
+@kernel
+def entrypoint():
+    foo()
