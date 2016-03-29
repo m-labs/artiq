b59283f85eeb956938116ee9261ef5ecb9c63d3e
Sebastien Bourdeauducq <sb@m-labs.hk>
2016-03-29 17:10:37 +0800

    gui/applets: use a better default size, make minimum size proportional to font

--- artiq/gui/applets.py
+++ artiq/gui/applets.py
@@ -89,7 +89,10 @@ class _AppletDock(QDockWidgetCloseDetect):
     def __init__(self, datasets_sub, uid, name, command):
         QDockWidgetCloseDetect.__init__(self, "Applet: " + name)
         self.setObjectName("applet" + str(uid))
-        self.setMinimumSize(QtCore.QSize(100, 100))
+
+        qfm = QtGui.QFontMetrics(self.font())
+        self.setMinimumSize(20*qfm.averageCharWidth(), 5*qfm.lineSpacing())
+        self.resize(40*qfm.averageCharWidth(), 10*qfm.lineSpacing())
 
         self.datasets_sub = datasets_sub
         self.applet_name = name
