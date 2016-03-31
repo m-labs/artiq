63121e4faf72bda3d8d7b0728851cb0c0f5d2705
whitequark <whitequark@whitequark.org>
2016-03-31 09:17:34 +0000

    test: XFAIL lit/devirtualization/*.

--- artiq/test/lit/devirtualization/method.py
+++ artiq/test/lit/devirtualization/method.py
@@ -1,5 +1,6 @@
 # RUN: env ARTIQ_DUMP_IR=%t %python -m artiq.compiler.testbench.embedding +compile %s 2>%t
 # RUN: OutputCheck %s --file-to-check=%t.txt
+# XFAIL: *
 
 from artiq.language.core import *
 from artiq.language.types import *
