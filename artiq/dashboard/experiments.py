b769403667361089fa7d4462fad42d6e65f6d5ec
Sebastien Bourdeauducq <sb@m-labs.hk>
2016-06-19 09:31:19 +0800

    dashboard: larger experiment initial window size

--- artiq/dashboard/experiments.py
+++ artiq/dashboard/experiments.py
@@ -201,7 +201,7 @@ class _ExperimentDock(QtWidgets.QMdiSubWindow):
     def __init__(self, manager, expurl):
         QtWidgets.QMdiSubWindow.__init__(self)
         qfm = QtGui.QFontMetrics(self.font())
-        self.resize(90*qfm.averageCharWidth(), 30*qfm.lineSpacing())
+        self.resize(100*qfm.averageCharWidth(), 30*qfm.lineSpacing())
         self.setWindowTitle(expurl)
         self.setWindowIcon(QtWidgets.QApplication.style().standardIcon(
             QtWidgets.QStyle.SP_FileDialogContentsView))
