078a9abeb95e32de949d225b7f9680b817992ba0
Sebastien Bourdeauducq <sb@m-labs.hk>
2016-07-09 10:07:19 +0800

    rtio: do not reset DDS and SPI PHYs on RTIO reset (#503)

--- artiq/gateware/rtio/phy/spi.py
+++ artiq/gateware/rtio/phy/spi.py
@@ -6,7 +6,7 @@ from artiq.gateware.rtio.phy.wishbone import RT2WB
 
 class SPIMaster(Module):
     def __init__(self, pads, **kwargs):
-        self.submodules._ll = ClockDomainsRenamer("rio")(
+        self.submodules._ll = ClockDomainsRenamer("rio_phy")(
             SPIMasterWB(pads, **kwargs))
         self.submodules._rt2wb = RT2WB(2, self._ll.bus)
         self.rtlink = self._rt2wb.rtlink
