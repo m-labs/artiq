972a74219dfa7e2c11ed3247f2de42998f1edbbc
Sebastien Bourdeauducq <sb@m-labs.hk>
2016-07-13 11:26:35 +0800

    monkey-patch Python 3.5.2 to disable broken asyncio.base_events._ipaddr_info optimization (#506)

--- artiq/monkey_patches.py
+++ artiq/monkey_patches.py
@@ -155,3 +155,12 @@ if sys.version_info[:3] == (3, 5, 1):
     
     from asyncio import proactor_events
     proactor_events._ProactorBaseWritePipeTransport._loop_writing = _loop_writing
+
+
+if sys.version_info[:3] == (3, 5, 2):
+    import asyncio
+
+    # See https://github.com/m-labs/artiq/issues/506
+    def _ipaddr_info(host, port, family, type, proto):
+        return None
+    asyncio.base_events._ipaddr_info = _ipaddr_info
