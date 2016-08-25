c38e2cda9de7d02d53e74ab6ed5664f4041cefa7
Robert Jordens <rj@m-labs.hk>
2016-08-25 22:21:29 +0200

    doc/manual: no analytics_id with rtd

--- doc/manual/conf.py
+++ doc/manual/conf.py
@@ -130,9 +130,7 @@ html_theme = 'sphinx_rtd_theme'
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
 # documentation.
-html_theme_options = {
-  'analytics_id': 'UA-75602447-2',
-}
+html_theme_options = {}
 
 # Add any paths that contain custom themes here, relative to this directory.
 #html_theme_path = []
