b52ecda1d532b91d5feec6470e571524efbf5ada
whitequark <whitequark@whitequark.org>
2016-10-06 18:05:38 +0000

    runtime: make memory map saner.

--- artiq/runtime.rs/src/kernel.rs
+++ artiq/runtime.rs/src/kernel.rs
@@ -2,7 +2,7 @@ use core::ptr;
 use board::csr;
 use mailbox;
 
-const KERNELCPU_EXEC_ADDRESS:    usize = 0x42000000;
+const KERNELCPU_EXEC_ADDRESS:    usize = 0x40400000;
 const KERNELCPU_LAST_ADDRESS:    usize = (0x4fffffff - 1024*1024);
 const KSUPPORT_HEADER_SIZE:      usize = 0x80;
 
