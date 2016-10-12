0d1ed247e26b2d0edc49d848a7b46978fa0f0247
Robert Jordens <rj@m-labs.hk>
2016-10-12 15:45:41 +0200

    phaser: tweak sawg example

--- artiq/examples/phaser/repository/sawg.py
+++ artiq/examples/phaser/repository/sawg.py
@@ -14,19 +14,18 @@ class SAWGTest(EnvExperiment):
     @kernel
     def run(self):
         self.core.break_realtime()
-        delay(100*us)
 
         self.sawg0.set_amplitude(.1)
-        self.sawg1.set_amplitude(-.9)
-        self.sawg2.set_amplitude(.5)
-        self.sawg3.set_amplitude(.5)
-        self.sawg0.set_frequency(1*MHz)
-        self.sawg1.set_frequency(10*MHz)
-        self.sawg2.set_frequency(20*MHz)
-        self.sawg3.set_frequency(20*MHz)
+        self.sawg0.set_frequency(10*MHz)
         self.sawg0.set_phase(0.)
+        self.sawg1.set_amplitude(-.9)
+        self.sawg1.set_frequency(20*MHz)
         self.sawg1.set_phase(0.)
+        self.sawg2.set_amplitude(.5)
+        self.sawg2.set_frequency(30*MHz)
         self.sawg2.set_phase(0.)
+        self.sawg3.set_amplitude(.5)
+        self.sawg3.set_frequency(30*MHz)
         self.sawg3.set_phase(.5)
 
         for i in range(10):
