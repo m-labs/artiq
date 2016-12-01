46dbc44c8f90d19410b86e293dbbfe06495f64ec
Sebastien Bourdeauducq <sb@m-labs.hk>
2016-12-01 16:30:29 +0800

    rtio: export DMA and CRIInterconnectShared

--- artiq/gateware/rtio/__init__.py
+++ artiq/gateware/rtio/__init__.py
@@ -1,4 +1,5 @@
-from artiq.gateware.rtio.cri import KernelInitiator, CRIDecoder
+from artiq.gateware.rtio.cri import KernelInitiator, CRIInterconnectShared
 from artiq.gateware.rtio.core import Channel, LogChannel, Core
 from artiq.gateware.rtio.analyzer import Analyzer
 from artiq.gateware.rtio.moninj import MonInj
+from artiq.gateware.rtio.dma import DMA
