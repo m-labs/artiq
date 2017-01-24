209be7320483539b292c4d3d1ef04ff7bc77d208
whitequark <whitequark@whitequark.org>
2017-01-24 22:33:09 +0000

    firmware: simplify ksupport build script.

--- artiq/firmware/libksupport/build.rs
+++ artiq/firmware/libksupport/build.rs
@@ -1,17 +1,6 @@
-use std::env;
-use std::path::Path;
-use std::io::{BufRead, BufReader};
-use std::fs::File;
+extern crate build_artiq;
 
 fn main() {
-    let out_dir = env::var("BUILDINC_DIRECTORY").unwrap();
-    let cfg_path = Path::new(&out_dir).join("generated").join("rust-cfg");
-    println!("cargo:rerun-if-changed={}", cfg_path.to_str().unwrap());
-
-    let f = BufReader::new(File::open(&cfg_path).unwrap());
-    for line in f.lines() {
-        println!("cargo:rustc-cfg={}", line.unwrap());
-    }
-
+    build_artiq::misoc_registers();
     println!("cargo:rustc-cfg={}", "ksupport");
 }
