c83e15d040c0d021913673fd74d3d1b3944abc4e
Sebastien Bourdeauducq <sb@m-labs.hk>
2017-05-22 00:06:03 +0800

    artiq_corelog: cleanup

--- artiq/frontend/artiq_corelog.py
+++ artiq/frontend/artiq_corelog.py
@@ -4,13 +4,12 @@ import argparse
 
 from artiq.tools import verbosity_args, init_logger
 from artiq.master.databases import DeviceDB
-from artiq.master.worker_db import DeviceManager
 from artiq.coredevice.comm_mgmt import CommMgmt
 
 
 def get_argparser():
     parser = argparse.ArgumentParser(description="ARTIQ core device "
-                                                 "log download tool")
+                                                 "log tool")
     verbosity_args(parser)
     parser.add_argument("--device-db", default="device_db.py",
                        help="device database file (default: '%(default)s')")
@@ -37,10 +36,10 @@ def get_argparser():
 def main():
     args = get_argparser().parse_args()
     init_logger(args)
-    device_mgr = DeviceManager(DeviceDB(args.device_db))
+
+    core_addr = DeviceDB(args.device_db).get("comm")["arguments"]["host"]
+    mgmt = CommMgmt(None, core_addr)
     try:
-        core_addr = device_mgr.get_desc("comm")["arguments"]["host"]
-        mgmt = CommMgmt(device_mgr, core_addr)
         if args.action == "set_level":
             mgmt.set_log_level(args.level)
         elif args.action == "set_uart_level":
@@ -50,7 +49,7 @@ def main():
         else:
             print(mgmt.get_log(), end="")
     finally:
-        device_mgr.close_devices()
+        mgmt.close()
 
 
 if __name__ == "__main__":
