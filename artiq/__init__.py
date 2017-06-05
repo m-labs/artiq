45f62d4e2df3b6674e41e432141ef95b2272659a
Sebastien Bourdeauducq <sb@m-labs.hk>
2017-06-05 13:18:12 +0800

    update versioneer

--- artiq/__init__.py
+++ artiq/__init__.py
@@ -5,3 +5,7 @@ del get_versions
 import os
 __artiq_dir__ = os.path.dirname(os.path.abspath(__file__))
 del os
+
+from ._version import get_versions
+__version__ = get_versions()['version']
+del get_versions
