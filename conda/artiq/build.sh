2b18943947356b39e78158148c6d962a6fe866e4
Sebastien Bourdeauducq <sb@m-labs.hk>
2017-06-06 13:34:23 +0800

    conda: use new noarch system

--- conda/artiq/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-ARTIQ_PREFIX=$PREFIX/lib/python3.5/site-packages/artiq
-
-$PYTHON setup.py install --single-version-externally-managed --record=record.txt
--- setup.py
+++ setup.py
@@ -19,27 +19,27 @@ requirements = [
 ]
 
 console_scripts = [
-    "artiq_client=artiq.frontend.artiq_client:main",
-    "artiq_compile=artiq.frontend.artiq_compile:main",
-    "artiq_coreanalyzer=artiq.frontend.artiq_coreanalyzer:main",
-    "artiq_coreconfig=artiq.frontend.artiq_coreconfig:main",
-    "artiq_corelog=artiq.frontend.artiq_corelog:main",
-    "artiq_ctlmgr=artiq.frontend.artiq_ctlmgr:main",
-    "artiq_influxdb=artiq.frontend.artiq_influxdb:main",
-    "artiq_master=artiq.frontend.artiq_master:main",
-    "artiq_mkfs=artiq.frontend.artiq_mkfs:main",
-    "artiq_rpctool=artiq.frontend.artiq_rpctool:main",
-    "artiq_run=artiq.frontend.artiq_run:main",
-    "artiq_flash=artiq.frontend.artiq_flash:main",
-    "lda_controller=artiq.frontend.lda_controller:main",
-    "novatech409b_controller=artiq.frontend.novatech409b_controller:main",
-    "pdq2_controller=artiq.frontend.pdq2_controller:main",
-    "thorlabs_tcube_controller=artiq.frontend.thorlabs_tcube_controller:main",
+    "artiq_client = artiq.frontend.artiq_client:main",
+    "artiq_compile = artiq.frontend.artiq_compile:main",
+    "artiq_coreanalyzer = artiq.frontend.artiq_coreanalyzer:main",
+    "artiq_coreconfig = artiq.frontend.artiq_coreconfig:main",
+    "artiq_corelog = artiq.frontend.artiq_corelog:main",
+    "artiq_ctlmgr = artiq.frontend.artiq_ctlmgr:main",
+    "artiq_influxdb = artiq.frontend.artiq_influxdb:main",
+    "artiq_master = artiq.frontend.artiq_master:main",
+    "artiq_mkfs = artiq.frontend.artiq_mkfs:main",
+    "artiq_rpctool = artiq.frontend.artiq_rpctool:main",
+    "artiq_run = artiq.frontend.artiq_run:main",
+    "artiq_flash = artiq.frontend.artiq_flash:main",
+    "lda_controller = artiq.frontend.lda_controller:main",
+    "novatech409b_controller = artiq.frontend.novatech409b_controller:main",
+    "pdq2_controller = artiq.frontend.pdq2_controller:main",
+    "thorlabs_tcube_controller = artiq.frontend.thorlabs_tcube_controller:main",
 ]
 
 gui_scripts = [
-    "artiq_browser=artiq.frontend.artiq_browser:main",
-    "artiq_dashboard=artiq.frontend.artiq_dashboard:main",
+    "artiq_browser = artiq.frontend.artiq_browser:main",
+    "artiq_dashboard = artiq.frontend.artiq_dashboard:main",
 ]
 
 setup(
