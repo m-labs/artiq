1fb3995ffc69fa33bc311fdaa6ab849f435d748d
Robert Jordens <rj@m-labs.hk>
2017-06-12 20:07:25 +0200

    Revert "fir/ParallelHBFUpsampler: add headroom (gain=2)"
    This reverts commit 6ac9d0c41efba9d9a6f85754b8ec131a9e74f0cc.
    
    Overshooting behavior must to be handled outside the FIR.

--- artiq/gateware/dsp/fir.py
+++ artiq/gateware/dsp/fir.py
@@ -163,7 +163,7 @@ class ParallelHBFUpsampler(Module):
         i = [self.i]
         for coeff in coefficients:
             self.parallelism *= 2
-            hbf = ParallelFIR(coeff, self.parallelism, width + 1, **kwargs)
+            hbf = ParallelFIR(coeff, self.parallelism, width, **kwargs)
             self.submodules += hbf
             self.comb += [a.eq(b) for a, b in zip(hbf.i[::2], i)]
             i = hbf.o
