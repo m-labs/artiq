f8f84baff157062778c8dab68a85ae2094e0ae05
Thao <ngchristina199678@gmail.com>
2017-08-04 17:16:54 +0800

    add test_compile.py for artiq_compile

---
+++ artiq/test/coredevice/test_compile.py
@@ -0,0 +1,32 @@
+import os, shutil
+import subprocess
+import unittest
+import tempfile
+from artiq.coredevice.comm_mgmt import CommMgmt
+from artiq.test.hardware_testbench import ExperimentCase
+from artiq.experiment import *
+
+
+class LOG(EnvExperiment):
+    def build(self):
+        self.setattr_device("core")
+
+    @kernel
+    def run(self):
+        self.core.reset()
+        core_log("blahblah123")
+
+
+
+class TestCompile(ExperimentCase):
+    def test_compile(self):
+        core_addr = self.device_mgr.get_desc("core")["arguments"]["host"]
+        mgmt = CommMgmt(core_addr)
+        mgmt.clear_log()
+        with tempfile.TemporaryDirectory() as tmp:
+            subprocess.call(["artiq_compile", "-e", "LOG", "-o", "hehe.elf", "test_compile.py"])
+            subprocess.call(["artiq_run", "hehe.elf"])
+            shutil.move(os.getcwd() + '\hehe.elf', tmp + '\hehe.elf')
+        log = mgmt.get_log()
+        self.assertIn("blahblah123", log)
+        mgmt.close()
