4baf17cebe94e2348d3d18727fd653e04bbab31b
Sebastien Bourdeauducq <sb@m-labs.hk>
2017-09-05 21:46:03 +0800

    libboard: generate HMC7043 register write list

--- artiq/firmware/libboard/build.rs
+++ artiq/firmware/libboard/build.rs
@@ -1,5 +1,29 @@
 extern crate build_artiq;
 
+use std::env;
+use std::fs::File;
+use std::io::Write;
+use std::path::PathBuf;
+use std::process::Command;
+
+fn gen_hmc7043_writes() {
+    println!("cargo:rerun-if-changed=hmc7043_gen_writes.py");
+    println!("cargo:rerun-if-changed=hmc7043_guiexport_10gbps.py");
+
+    let hmc7043_writes =
+        Command::new("python3")
+                .arg("hmc7043_gen_writes.py")
+                .arg("hmc7043_guiexport_10gbps.py")
+                .output()
+                .ok()
+                .and_then(|o| String::from_utf8(o.stdout).ok())
+                .unwrap();
+    let out_dir = PathBuf::from(env::var("OUT_DIR").unwrap());
+    let mut f = File::create(out_dir.join("hmc7043_writes.rs")).unwrap();
+    write!(f, "{}", hmc7043_writes).unwrap();
+}
+
 fn main() {
     build_artiq::misoc_cfg();
+    gen_hmc7043_writes();
 }
--- artiq/firmware/libboard/hmc830_7043.rs
+++ artiq/firmware/libboard/hmc830_7043.rs
@@ -1,3 +1,5 @@
+include!(concat!(env!("OUT_DIR"), "/hmc7043_writes.rs"));
+
 pub fn init() -> Result<(), &'static str> {
     error!("HMC830/7043 support is not implemented");
     Ok(())
