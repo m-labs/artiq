67997d8955e816f426ad9f3ace0d20cbb7dad7a9
whitequark <whitequark@whitequark.org>
2017-10-01 18:28:31 +0000

    compiler: correct semantics of integer % operator (#830).

--- artiq/test/lit/integration/arithmetics.py
+++ artiq/test/lit/integration/arithmetics.py
@@ -1,5 +1,5 @@
-# RUN: %python -m artiq.compiler.testbench.jit %s
 # RUN: %python %s
+# RUN: %python -m artiq.compiler.testbench.jit %s
 # REQUIRES: exceptions
 
 assert -(-1) == 1
@@ -20,10 +20,16 @@ assert 3 % 2 == 1
 assert -3 % 2 == 1
 assert 3 % -2 == -1
 assert -3 % -2 == -1
-assert 3.0 % 2.0 == 1.0
-assert -3.0 % 2.0 == 1.0
-assert 3.0 % -2.0 == -1.0
-assert -3.0 % -2.0 == -1.0
+assert -1 % 8 == 7
+#ARTIQ#assert int64(3) % 2 == 1
+#ARTIQ#assert int64(-3) % 2 == 1
+#ARTIQ#assert int64(3) % -2 == -1
+#ARTIQ#assert int64(-3) % -2 == -1
+assert -1 % 8 == 7
+# assert 3.0 % 2.0 == 1.0
+# assert -3.0 % 2.0 == 1.0
+# assert 3.0 % -2.0 == -1.0
+# assert -3.0 % -2.0 == -1.0
 assert 3 ** 2 == 9
 assert 3.0 ** 2.0 == 9.0
 assert 9.0 ** 0.5 == 3.0
@@ -36,5 +42,21 @@ assert 0x18 & 0x0f == 0x08
 assert 0x18 | 0x0f == 0x1f
 assert 0x18 ^ 0x0f == 0x17
 
-assert [1] + [2] == [1, 2]
-assert [1] * 3 == [1, 1, 1]
+try:
+    1 / 0
+except ZeroDivisionError:
+    pass
+else:
+    assert False
+try:
+    1 // 0
+except ZeroDivisionError:
+    pass
+else:
+    assert False
+try:
+    1 % 0
+except ZeroDivisionError:
+    pass
+else:
+    assert False
