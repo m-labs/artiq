cfb41e71a89d7e469b9571c6e76686b8b8b3df6a
Sebastien Bourdeauducq <sb@m-labs.hk>
2017-11-28 01:23:33 +0800

    libboard: print serwb settings at the debug log level

--- artiq/firmware/libboard/serwb.rs
+++ artiq/firmware/libboard/serwb.rs
@@ -17,29 +17,25 @@ pub fn wait_init() {
         // proceed anyway
     }
     
-    // Show AMC serwb settings
     unsafe {
-        info!("AMC serwb settings:");
-        info!("  delay_min_found: {}", csr::serwb_phy_amc::control_delay_min_found_read());
-        info!("  delay_min: {}", csr::serwb_phy_amc::control_delay_min_read());
-        info!("  delay_max_found: {}", csr::serwb_phy_amc::control_delay_max_found_read());
-        info!("  delay_max: {}", csr::serwb_phy_amc::control_delay_max_read());
-        info!("  delay: {}", csr::serwb_phy_amc::control_delay_read());
-        info!("  bitslip: {}", csr::serwb_phy_amc::control_bitslip_read());
-        info!("  ready: {}", csr::serwb_phy_amc::control_ready_read());
-        info!("  error: {}", csr::serwb_phy_amc::control_error_read());
-    }
+        debug!("AMC serwb settings:");
+        debug!("  delay_min_found: {}", csr::serwb_phy_amc::control_delay_min_found_read());
+        debug!("  delay_min: {}", csr::serwb_phy_amc::control_delay_min_read());
+        debug!("  delay_max_found: {}", csr::serwb_phy_amc::control_delay_max_found_read());
+        debug!("  delay_max: {}", csr::serwb_phy_amc::control_delay_max_read());
+        debug!("  delay: {}", csr::serwb_phy_amc::control_delay_read());
+        debug!("  bitslip: {}", csr::serwb_phy_amc::control_bitslip_read());
+        debug!("  ready: {}", csr::serwb_phy_amc::control_ready_read());
+        debug!("  error: {}", csr::serwb_phy_amc::control_error_read());
 
-    // Show RTM serwb settings
-    unsafe {
-        info!("RTM serwb settings:");
-        info!("  delay_min_found: {}", csr::serwb_phy_rtm::control_delay_min_found_read());
-        info!("  delay_min: {}", csr::serwb_phy_rtm::control_delay_min_read());
-        info!("  delay_max_found: {}", csr::serwb_phy_rtm::control_delay_max_found_read());
-        info!("  delay_max: {}", csr::serwb_phy_rtm::control_delay_max_read());
-        info!("  delay: {}", csr::serwb_phy_rtm::control_delay_read());
-        info!("  bitslip: {}", csr::serwb_phy_rtm::control_bitslip_read());
-        info!("  ready: {}", csr::serwb_phy_rtm::control_ready_read());
-        info!("  error: {}", csr::serwb_phy_rtm::control_error_read());
+        debug!("RTM serwb settings:");
+        debug!("  delay_min_found: {}", csr::serwb_phy_rtm::control_delay_min_found_read());
+        debug!("  delay_min: {}", csr::serwb_phy_rtm::control_delay_min_read());
+        debug!("  delay_max_found: {}", csr::serwb_phy_rtm::control_delay_max_found_read());
+        debug!("  delay_max: {}", csr::serwb_phy_rtm::control_delay_max_read());
+        debug!("  delay: {}", csr::serwb_phy_rtm::control_delay_read());
+        debug!("  bitslip: {}", csr::serwb_phy_rtm::control_bitslip_read());
+        debug!("  ready: {}", csr::serwb_phy_rtm::control_ready_read());
+        debug!("  error: {}", csr::serwb_phy_rtm::control_error_read());
     }
 }
