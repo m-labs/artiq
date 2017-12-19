231bf77b43f2aef5eb8f7830ebbf490970bb55fd
whitequark <whitequark@whitequark.org>
2017-12-19 15:51:03 +0000

    runtime: update smoltcp.

--- artiq/firmware/runtime/lib.rs
+++ artiq/firmware/runtime/lib.rs
@@ -114,9 +114,14 @@ fn startup() {
     let net_device = unsafe { ethmac::EthernetDevice::new() };
     // let net_device = smoltcp::phy::EthernetTracer::new(net_device, _net_trace_writer);
     let mut neighbor_cache_storage = [None; 8];
-    let neighbor_cache = smoltcp::iface::NeighborCache::new(&mut neighbor_cache_storage[..]);
-    let mut interface  = smoltcp::iface::EthernetInterface::new(
-        net_device, neighbor_cache, hardware_addr, [IpCidr::new(protocol_addr, 0)], None);
+    let neighbor_cache =
+        smoltcp::iface::NeighborCache::new(&mut neighbor_cache_storage[..]);
+    let mut interface  =
+        smoltcp::iface::EthernetInterfaceBuilder::new(net_device)
+                       .neighbor_cache(neighbor_cache)
+                       .ethernet_addr(hardware_addr)
+                       .ip_addrs([IpCidr::new(protocol_addr, 0)])
+                       .finalize();
 
     let mut scheduler = sched::Scheduler::new();
     let io = scheduler.io();
