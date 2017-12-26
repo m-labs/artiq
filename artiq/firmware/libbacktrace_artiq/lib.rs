6e341da3a1f21b0e19231f13e50767337bb08938
whitequark <whitequark@whitequark.org>
2017-12-26 02:31:28 +0000

    runtime: simplify. NFC.

--- artiq/firmware/libbacktrace_artiq/lib.rs
+++ artiq/firmware/libbacktrace_artiq/lib.rs
@@ -1,5 +1,4 @@
-#![feature(libc, panic_unwind, never_type)]
-#![allow(non_upper_case_globals, non_camel_case_types)]
+#![feature(libc, panic_unwind)]
 #![no_std]
 
 extern crate unwind;
@@ -7,13 +6,6 @@ extern crate libc;
 
 use unwind as uw;
 use libc::c_void;
-
-type _Unwind_Trace_Fn = extern "C" fn(*mut uw::_Unwind_Context, *mut c_void)
-                                     -> uw::_Unwind_Reason_Code;
-extern {
-    fn _Unwind_Backtrace(trace_fn: _Unwind_Trace_Fn, arg: *mut c_void)
-                        -> uw::_Unwind_Reason_Code;
-}
 
 pub fn backtrace<F>(mut f: F) -> Result<(), uw::_Unwind_Reason_Code>
     where F: FnMut(usize) -> ()
@@ -30,7 +22,7 @@ pub fn backtrace<F>(mut f: F) -> Result<(), uw::_Unwind_Reason_Code>
     }
 
     unsafe {
-        match _Unwind_Backtrace(trace::<F>, &mut f as *mut _ as *mut c_void) {
+        match uw::_Unwind_Backtrace(trace::<F>, &mut f as *mut _ as *mut c_void) {
             uw::_URC_NO_REASON => Ok(()),
             err => Err(err)
         }
