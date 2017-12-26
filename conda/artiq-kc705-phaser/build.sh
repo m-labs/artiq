26dbf0841cf1afe519c518f2be98c50482d22f56
whitequark <whitequark@whitequark.org>
2017-12-26 21:50:31 +0000

    conda: ship runtime.elf in board-specific packages.
    This is so that backtraces may be symbolized.

--- conda/artiq-kc705-phaser/build.sh
+++ conda/artiq-kc705-phaser/build.sh
@@ -9,6 +9,6 @@ mkdir -p $SOC_PREFIX
 V=1 $PYTHON -m artiq.gateware.targets.kc705_phaser --toolchain vivado $MISOC_EXTRA_VIVADO_CMDLINE
 cp misoc_phaser_kc705/gateware/top.bit $SOC_PREFIX
 cp misoc_phaser_kc705/software/bios/bios.bin $SOC_PREFIX
-cp misoc_phaser_kc705/software/runtime/runtime.fbi $SOC_PREFIX
+cp misoc_phaser_kc705/software/runtime/runtime.{elf,fbi} $SOC_PREFIX
 
 wget -P $SOC_PREFIX https://raw.githubusercontent.com/jordens/bscan_spi_bitstreams/single-tap/bscan_spi_xc7k325t.bit
