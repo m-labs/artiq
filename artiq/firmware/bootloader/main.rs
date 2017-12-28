35058781769d36fc6931b3ec56a90b7c037a39b0
whitequark <whitequark@whitequark.org>
2017-12-28 14:51:20 +0000

    bootloader: add basic network support.

--- artiq/firmware/bootloader/main.rs
+++ artiq/firmware/bootloader/main.rs
@@ -1,16 +1,18 @@
 #![no_std]
-#![feature(lang_items)]
+#![feature(lang_items, compiler_builtins_lib)]
 
-extern crate rlibc;
+extern crate compiler_builtins;
 extern crate crc;
 extern crate byteorder;
+extern crate smoltcp;
 #[macro_use]
 extern crate board;
 
 use core::{ptr, slice};
 use crc::crc32;
 use byteorder::{ByteOrder, BigEndian};
-use board::{boot, cache};
+use smoltcp::wire::{EthernetAddress, IpAddress, IpCidr};
+use board::{boot, cache, clock, config, ethmac};
 use board::uart_console::Console;
 
 fn check_integrity() -> bool {
@@ -28,7 +30,7 @@ fn check_integrity() -> bool {
     }
 }
 
-unsafe fn memory_test(total: &mut usize, wrong: &mut usize) -> bool {
+fn memory_test(total: &mut usize, wrong: &mut usize) -> bool {
     const MEMORY: *mut u32 = board::mem::MAIN_RAM_BASE as *mut u32;
 
     *total = 0;
@@ -43,7 +45,7 @@ unsafe fn memory_test(total: &mut usize, wrong: &mut usize) -> bool {
         ) => ({
             $prepare;
             for $i in $range {
-                ptr::write_volatile(MEMORY.offset($index as isize), $data);
+                unsafe { ptr::write_volatile(MEMORY.offset($index as isize), $data) };
                 *total += 1;
             }
 
@@ -52,7 +54,7 @@ unsafe fn memory_test(total: &mut usize, wrong: &mut usize) -> bool {
 
             $prepare;
             for $i in $range {
-                if ptr::read_volatile(MEMORY.offset($index as isize)) != $data {
+                if unsafe { ptr::read_volatile(MEMORY.offset($index as isize)) } != $data {
                     *wrong += 1;
                 }
             }
@@ -95,7 +97,7 @@ fn startup() -> bool {
     }
 
     let (mut total, mut wrong) = (0, 0);
-    if unsafe { memory_test(&mut total, &mut wrong) } {
+    if memory_test(&mut total, &mut wrong) {
         println!("Memory test passed");
     } else {
         println!("Memory test failed ({}/{} words incorrect)", wrong, total);
@@ -105,33 +107,78 @@ fn startup() -> bool {
     true
 }
 
-unsafe fn flash_boot() {
+fn flash_boot() {
     const FIRMWARE: *mut u8 = board::mem::FLASH_BOOT_ADDRESS as *mut u8;
     const MAIN_RAM: *mut u8 = board::mem::MAIN_RAM_BASE as *mut u8;
 
     println!("Booting from flash...");
 
-    let header = slice::from_raw_parts(FIRMWARE, 8);
+    let header = unsafe { slice::from_raw_parts(FIRMWARE, 8) };
     let length = BigEndian::read_u32(&header[0..]) as usize;
     let expected_crc = BigEndian::read_u32(&header[4..]);
 
     if length == 0xffffffff {
         println!("No firmware present");
         return
+    } else if length > 4 * 1024 * 1024 {
+        println!("Firmware too large (is it corrupted?)");
+        return
     }
 
-    let firmware_in_flash = slice::from_raw_parts(FIRMWARE.offset(8), length);
+    let firmware_in_flash = unsafe { slice::from_raw_parts(FIRMWARE.offset(8), length) };
     let actual_crc = crc32::checksum_ieee(firmware_in_flash);
 
     if actual_crc == expected_crc {
-        let firmware_in_sdram = slice::from_raw_parts_mut(MAIN_RAM, length);
+        let firmware_in_sdram = unsafe { slice::from_raw_parts_mut(MAIN_RAM, length) };
         firmware_in_sdram.copy_from_slice(firmware_in_flash);
 
         println!("Starting firmware.");
-        boot::jump(MAIN_RAM as usize);
+        unsafe { boot::jump(MAIN_RAM as usize) }
     } else {
-        println!("Firmware CRC failed (actual {:08x}, expected {:08x}",
+        println!("Firmware CRC failed (actual {:08x}, expected {:08x})",
                  actual_crc, expected_crc);
+    }
+}
+
+fn network_boot() {
+    println!("Initializing network...");
+
+    let eth_addr = match config::read_str("mac", |r| r.map(|s| s.parse())) {
+        Ok(Ok(addr)) => addr,
+        _ => EthernetAddress([0x02, 0x00, 0x00, 0x00, 0x00, 0x01])
+    };
+
+    let ip_addr = match config::read_str("ip", |r| r.map(|s| s.parse())) {
+        Ok(Ok(addr)) => addr,
+        _ => IpAddress::v4(192, 168, 1, 50)
+    };
+
+    println!("Using MAC address {} and IP address {}", eth_addr, ip_addr);
+
+    let net_device = unsafe { ethmac::EthernetDevice::new() };
+    let mut neighbor_cache_storage = [None; 2];
+    let neighbor_cache =
+        smoltcp::iface::NeighborCache::new(&mut neighbor_cache_storage[..]);
+    let mut ip_addrs = [IpCidr::new(ip_addr, 0)];
+    let mut interface  =
+        smoltcp::iface::EthernetInterfaceBuilder::new(net_device)
+                       .neighbor_cache(neighbor_cache)
+                       .ethernet_addr(eth_addr)
+                       .ip_addrs(&mut ip_addrs[..])
+                       .finalize();
+
+    let mut socket_set_storage = [];
+    let mut sockets =
+        smoltcp::socket::SocketSet::new(&mut socket_set_storage[..]);
+
+    println!("Waiting for connections...");
+
+    loop {
+        match interface.poll(&mut sockets, clock::get_ms()) {
+            Ok(_) => (),
+            Err(smoltcp::Error::Unrecognized) => (),
+            Err(err) => println!("Network error: {}", err)
+        }
     }
 }
 
@@ -150,7 +197,8 @@ pub extern fn main() -> i32 {
 
     if startup() {
         println!("");
-        unsafe { flash_boot() };
+        flash_boot();
+        network_boot();
     } else {
         println!("Halting.");
     }
@@ -161,6 +209,12 @@ pub extern fn main() -> i32 {
 #[no_mangle]
 pub extern fn exception(vect: u32, _regs: *const u32, pc: u32, ea: u32) {
     panic!("exception {} at PC {:#08x}, EA {:#08x}", vect, pc, ea)
+}
+
+#[no_mangle]
+pub extern fn abort() {
+    println!("aborted");
+    loop {}
 }
 
 #[no_mangle]
