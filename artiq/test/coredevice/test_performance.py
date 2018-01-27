67625fe9120b1952f691b3d4e5b632ecf265bf5d
Sebastien Bourdeauducq <sb@m-labs.hk>
2018-01-28 01:02:03 +0800

    test: check kernel overhead credibility

--- artiq/test/coredevice/test_performance.py
+++ artiq/test/coredevice/test_performance.py
@@ -79,4 +79,5 @@ class KernelOverheadTest(ExperimentCase):
         exp = self.create(_KernelOverhead)
         kernel_overhead = exp.kernel_overhead()
         print(kernel_overhead, "s")
+        self.assertGreater(kernel_overhead, 0.001)
         self.assertLess(kernel_overhead, 0.5)
