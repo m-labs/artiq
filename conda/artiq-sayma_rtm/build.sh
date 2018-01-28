9a94482c6ea712865314f6683cbff515e2b7b3cb
whitequark <whitequark@whitequark.org>
2018-01-28 15:18:52 +0000

    conda: fix typo in 885ab409.

--- conda/artiq-sayma_rtm/build.sh
+++ conda/artiq-sayma_rtm/build.sh
@@ -4,5 +4,5 @@ SOC_PREFIX=$SP_DIR/artiq/binaries/sayma_rtm
 mkdir -p $SOC_PREFIX
 
 $PYTHON -m artiq.gateware.targets.sayma_rtm
-cp artiq_sayma/rtm/rtm.bit $SOC_PREFIX
-cp artiq_sayma/rtm/rtm_csr.csv $SOC_PREFIX
+cp artiq_sayma/rtm_gateware/rtm.bit $SOC_PREFIX
+cp artiq_sayma/rtm_gateware/rtm_csr.csv $SOC_PREFIX
