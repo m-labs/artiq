df177bfd5be2264946f060ea625c9f03b6906d76
Sebastien Bourdeauducq <sb@m-labs.hk>
2018-02-13 20:38:48 +0800

    use new misoc identifier

--- artiq/firmware/libboard/ident.rs
+++ artiq/firmware/libboard/ident.rs
@@ -1,13 +1,15 @@
-use core::{ptr, cmp, str};
+use core::{cmp, str};
 use csr;
 
 pub fn read(buf: &mut [u8]) -> &str {
     unsafe {
-        let len = ptr::read_volatile(csr::IDENTIFIER_MEM_BASE);
-        let len = cmp::min(len as usize, buf.len());
+        csr::identifier::address_write(0);
+        let len = csr::identifier::data_read();
+        let len = cmp::min(len, buf.len() as u8);
         for i in 0..len {
-            buf[i] = ptr::read_volatile(csr::IDENTIFIER_MEM_BASE.offset(1 + i as isize)) as u8
+            csr::identifier::address_write(1 + i);
+            buf[i as usize] = csr::identifier::data_read();
         }
-        str::from_utf8_unchecked(&buf[..len])
+        str::from_utf8_unchecked(&buf[..len as usize])
     }
 }
