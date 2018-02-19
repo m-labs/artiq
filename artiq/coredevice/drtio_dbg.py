4b4090518b3975f6c52300a0b57167479287eb60
Sebastien Bourdeauducq <sb@m-labs.hk>
2018-02-19 15:14:06 +0800

    drtio: clean up remnants of removed debug functions

--- artiq/coredevice/drtio_dbg.py
+++ artiq/coredevice/drtio_dbg.py
@@ -9,21 +9,6 @@ from artiq.language.types import TTuple, TInt32, TInt64, TNone
 
 
 @syscall(flags={"nounwind", "nowrite"})
-def drtio_get_channel_state(channel: TInt32) -> TTuple([TInt32, TInt64]):
-    raise NotImplementedError("syscall not simulated")
-
-
-@syscall(flags={"nounwind", "nowrite"})
-def drtio_reset_channel_state(channel: TInt32) -> TNone:
-    raise NotImplementedError("syscall not simulated")
-
-
-@syscall(flags={"nounwind", "nowrite"})
-def drtio_get_fifo_space(channel: TInt32) -> TNone:
-    raise NotImplementedError("syscall not simulated")
-
-
-@syscall(flags={"nounwind", "nowrite"})
 def drtio_get_packet_counts(linkno: TInt32) -> TTuple([TInt32, TInt32]):
     raise NotImplementedError("syscall not simulated")
 
