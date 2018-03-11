c09269a323564e3024c5de6a23559877eeedbe53
whitequark <whitequark@whitequark.org>
2018-03-11 18:59:54 +0000

    conda: handle ARTIQ_VARIANT=phaser.

--- conda/artiq-board/build.sh
+++ conda/artiq-board/build.sh
@@ -3,7 +3,11 @@
 SOC_PREFIX=$SP_DIR/artiq/binaries/${ARTIQ_TARGET}-${ARTIQ_VARIANT}
 mkdir -p ${SOC_PREFIX}
 
-V=1 $PYTHON -m artiq.gateware.targets.kc705_dds -H ${ARTIQ_VARIANT} --toolchain vivado $MISOC_EXTRA_VIVADO_CMDLINE
+if [ "${ARTIQ_VARIANT}" = "phaser" ]; then
+  V=1 $PYTHON -m artiq.gateware.targets.kc705_phaser --toolchain vivado $MISOC_EXTRA_VIVADO_CMDLINE
+else
+  V=1 $PYTHON -m artiq.gateware.targets.kc705_dds -H ${ARTIQ_VARIANT} --toolchain vivado $MISOC_EXTRA_VIVADO_CMDLINE
+fi
 cp -v misoc_${ARTIQ_VARIANT}_kc705/gateware/top.bit $SOC_PREFIX
 cp -v misoc_${ARTIQ_VARIANT}_kc705/software/bios/bios.bin $SOC_PREFIX
 cp -v misoc_${ARTIQ_VARIANT}_kc705/software/runtime/runtime.{elf,fbi} $SOC_PREFIX
