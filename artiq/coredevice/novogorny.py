a27b5d88c2b482dd7c6c0d8eca88501d4e8d4b6d
hartytp <thomas.harty@physics.ox.ac.uk>
2018-03-19 10:58:14 +0000

    Novogorny driver, remove unused imports (#964)
    * Novogorny driver, remove unused imports
    
    * more unused imports
    
    * oops, one final one!

--- artiq/coredevice/novogorny.py
+++ artiq/coredevice/novogorny.py
@@ -1,11 +1,8 @@
-from numpy import int32, int64
 
 from artiq.language.core import kernel, delay, portable
-from artiq.language.units import us, ns
-from artiq.coredevice.ad9912_reg import *
+from artiq.language.units import ns
 
 from artiq.coredevice import spi2 as spi
-from artiq.coredevice import urukul
 
 
 SPI_CONFIG = (0*spi.SPI_OFFLINE | 0*spi.SPI_END |
