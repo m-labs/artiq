1e8cc731b6fe63bb661e6c88da8f9ce37aea6483
whitequark <whitequark@whitequark.org>
2018-04-21 19:38:40 +0000

    firmware: don't truncate queued RPCs (fixes #985).

---
+++ artiq/test/coredevice/test_stress.py
@@ -0,0 +1,29 @@
+import os
+import time
+import unittest
+
+from artiq.experiment import *
+from artiq.test.hardware_testbench import ExperimentCase
+
+
+artiq_low_latency = os.getenv("ARTIQ_LOW_LATENCY")
+
+
+class _Stress(EnvExperiment):
+    def build(self):
+        self.setattr_device("core")
+
+    @rpc(flags={"async"})
+    def sink(self, data):
+        pass
+
+    @kernel
+    def async_rpc(self, n):
+        for _ in range(n):
+            self.sink(b"")
+
+
+class StressTest(ExperimentCase):
+    def test_async_rpc(self):
+        exp = self.create(_Stress)
+        exp.async_rpc(16000)
