8d66d53e06a70f331b404da928d64d525e0efef0
Sebastien Bourdeauducq <sb@m-labs.hk>
2018-05-02 12:14:56 +0800

    Revert "tools/file_import: restore sys.modules. Closes #976"
    This reverts commit b48e782dd67282127a6bab96de5166a143e57f29.

--- artiq/tools.py
+++ artiq/tools.py
@@ -6,7 +6,6 @@ import collections
 import atexit
 import string
 import os
-from copy import copy
 
 import numpy as np
 
@@ -85,12 +84,8 @@ def file_import(filename, prefix="file_import_"):
     path = os.path.dirname(os.path.realpath(filename))
     sys.path.insert(0, path)
     try:
-        previous_modules = copy(sys.modules)
-        try:
-            loader = importlib.machinery.SourceFileLoader(modname, filename)
-            module = loader.load_module()
-        finally:
-            sys.modules = previous_modules
+        loader = importlib.machinery.SourceFileLoader(modname, filename)
+        module = loader.load_module()
     finally:
         sys.path.remove(path)
 
