e9189edc43000ad2e2cf7ecbd87573088a43f073
Paweł <marmeladapk@users.noreply.github.com>
2018-05-30 14:02:28 +0200

    Added second argument to DIO.add_STD in master and satellite variant of kasli (now builds properly)

--- artiq/gateware/targets/kasli.py
+++ artiq/gateware/targets/kasli.py
@@ -834,7 +834,7 @@ class Master(_MasterBase):
             phy = ttl_simple.Output(sc.led)
             self.submodules += phy
             self.rtio_channels.append(rtio.Channel.from_phy(phy))
-        eem.DIO.add_std(self, 0, ttl_serdes_7series.InOut_8X)
+        eem.DIO.add_std(self, 0, ttl_serdes_7series.InOut_8X, ttl_serdes_7series.InOut_8X)
 
         self.config["HAS_RTIO_LOG"] = None
         self.config["RTIO_LOG_CHANNEL"] = len(self.rtio_channels)
@@ -856,7 +856,7 @@ class Satellite(_SatelliteBase):
             phy = ttl_simple.Output(self.platform.request("sfp_ctl", i).led)
             self.submodules += phy
             self.rtio_channels.append(rtio.Channel.from_phy(phy))
-        eem.DIO.add_std(self, 0, ttl_serdes_7series.InOut_8X)
+        eem.DIO.add_std(self, 0, ttl_serdes_7series.InOut_8X, ttl_serdes_7series.InOut_8X)
 
         self.add_rtio(self.rtio_channels)
 
