0483b8d14cbaa4b53d6f5f830b964445b3cb185d
Sebastien Bourdeauducq <sb@m-labs.hk>
2018-06-28 17:03:32 +0800

    sayma_drtio: ditto

--- artiq/examples/sayma_drtio/repository/sines_drtio.py
+++ artiq/examples/sayma_drtio/repository/sines_drtio.py
@@ -17,6 +17,10 @@ class SAWGTestDRTIO(EnvExperiment):
         self.core.reset()
 
         for sawg in self.sawgs:
+                delay(1*ms)
+                sawg.reset()
+
+        for sawg in self.sawgs:
             delay(1*ms)
             sawg.amplitude1.set(.4)
             # Do not use a sub-multiple of oscilloscope sample rates.
