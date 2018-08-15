73cf071b1a88dabcfead6e9c8b1536b4d49c1d09
Sebastien Bourdeauducq <sb@m-labs.hk>
2018-08-16 00:33:52 +0800

    nix: update LLVM and llvmlite, remove clang (not needed for running)

--- nix/llvmlite.nix
+++ nix/llvmlite.nix
@@ -6,15 +6,15 @@ stdenv.mkDerivation rec {
   name = "llvmlite-${version}";
   src = fetchgit {
     url = "https://github.com/m-labs/llvmlite";
-    rev = "0f4ebae43c2d2a084deb8b693e3d42a7b2c82222";
-    sha256 = "0n90w0x001k0zyn8zz6jxc9i78agqv15m55vz2raw1y0rfw16mfl";
+    rev = "401dfb713166bdd2bc0d3ab2b7ebf12e7a434130";
+    sha256 = "1ci1pnpspv1pqz712yix1nmplq7568vpsr6gzzl3a33w9s0sw2nq";
     leaveDotGit = true;
   };
 
   buildInputs = [ makeWrapper python35 ncurses zlib llvm-or1k python35Packages.setuptools ];
 
   installPhase = ''
-    LLVM_CONFIG=${llvm-or1k}/llvm_or1k/bin/llvm-config
+    LLVM_CONFIG=${llvm-or1k}/bin/llvm-config
     python3 setup.py install --prefix=$out
   '';
 }
