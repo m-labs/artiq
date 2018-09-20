da01a03a6e4c45b0a552b02dec23e0fdf9393707
whitequark <whitequark@whitequark.org>
2018-09-20 10:01:32 +0000

    Fix b91822ff.

--- artiq/test/lit/escape/error_mutable_aug_asgn.py
+++ artiq/test/lit/escape/error_mutable_aug_asgn.py
@@ -6,7 +6,7 @@ from artiq.experiment import *
 @kernel
 def entrypoint():
     a = [1,2]
-    # CHECK-L: ${LINE:+2}: error: lists cannot be mutated in-place
+    # CHECK-L: ${LINE:+2}: error: values cannot be mutated in-place
     # CHECK-L: ${LINE:+1}: note: try using `a = a + [3,4]`
     a += [3,4]
 
