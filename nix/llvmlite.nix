421ad9c9168e1125bfd900d7fb0ec005d5c6d043
Sebastien Bourdeauducq <sb@m-labs.hk>
2018-12-22 14:01:52 +0800

    nix: bump llvmlite

--- nix/llvmlite.nix
+++ nix/llvmlite.nix
@@ -5,10 +5,10 @@ in
 stdenv.mkDerivation rec {
   name = "llvmlite-${version}";
   src = fetchFromGitHub {
-    rev = "401dfb713166bdd2bc0d3ab2b7ebf12e7a434130";
+    rev = "1d167be4eec5d6c32498952be8b3ac17dd30df8d";
     owner = "m-labs";
     repo = "llvmlite";
-    sha256 = "1hqahd87ihwgjsaxv0y2iywldi1zgyjxjfy3sy3rr1gnwvxb47xw";
+    sha256 = "0ranbjhcz2v3crmdbw1sxdwqwqbbm7dd53d8qaqb69ma9fkxy8x7";
   };
 
   buildInputs = [ makeWrapper python3 ncurses zlib llvm-or1k python3Packages.setuptools ];
