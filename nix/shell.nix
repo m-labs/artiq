e2799803cb396f6fe6ad7738a75a4bb1fa20b1be
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-01-01 23:35:55 +0800

    nix: do not install development packages in user environment

--- nix/shell.nix
+++ nix/shell.nix
@@ -3,9 +3,5 @@ let
   artiqpkgs = import ./default.nix { inherit pkgs; };
 in
   pkgs.mkShell {
-    buildInputs = with artiqpkgs; [ rustc cargo binutils-or1k llvm-or1k llvmlite migen misoc artiq ]
-      ++ (with pkgs; [ python3 python36Packages.jinja2 python36Packages.numpy ]); # for artiq the python lib...
-    shellHook = ''
-      export TARGET_AR=${artiqpkgs.binutils-or1k}/bin/or1k-linux-ar
-    '';
+    buildInputs = with artiqpkgs; [ binutils-or1k llvm-or1k llvmlite artiq ];
   }
