m-labs/artiq

commit f3323a35d56bc1a833ee9140f2a90f948ba7f324

artiq_influxdb: Unbreak after verbosity_args rename
This fixes commit f2c1d32e54e4a2a5a51952d67e96426d3b0a1de8.

artiq/frontend/artiq_influxdb.py
@@ -12,7 +12,7 @@ import numpy as np
 import aiohttp
 
 from artiq.tools import (
-    simple_network_args, verbosity_args, atexit_register_coroutine,
+    simple_network_args, add_common_args, atexit_register_coroutine,
     bind_address_from_args, init_logger, TaskObject
 )
 from artiq.protocols.sync_struct import Subscriber

commit 24b1b9a480a691133fff79846837632269148a9a

Add smoke test for frontend commands
This ensures that at least --help works for all the commands,
preventing regressions like that in f3323a35d56bc1a833ee9140f2a90f948ba7f324.

artiq/test/test_frontends.py
@@ -1,0 +1,35 @@
+"""Generic tests for frontend commands."""
+import subprocess
+import sys
+import unittest
+
+
+class TestFrontends(unittest.TestCase):
+    def test_help(self):
+        """Test --help as a simple smoke test against catastrophic breakage."""
+        commands = {
+            "aqctl": [
+                "corelog", "korad_ka3005p", "lda", "novatech409b",
+                "thorlabs_tcube"
+            ],
+            "artiq": [
+                "client", "compile", "coreanalyzer", "coremgmt", "ctlmgr",
+                "devtool", "flash", "influxdb", "master", "mkfs", "route",
+                "rpctool", "rtiomon", "run", "session"
+            ]
+        }
+
+        # Skip tests for GUI programs on headless CI environments.
+        try:
+            from PyQt5 import QtGui, QtWidgets
+            commands["artiq"] += ["browser", "dashboard"]
+        except ImportError:
+            pass
+
+        for module in (prefix + "_" + name
+                       for prefix, names in commands.items()
+                       for name in names):
+            subprocess.check_call(
+                [sys.executable, "-m", "artiq.frontend." + module, "--help"],
+                stdout=subprocess.DEVNULL,
+                stderr=subprocess.STDOUT)