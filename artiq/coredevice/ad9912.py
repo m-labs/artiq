1555fcf4ab13344b4165de4bc559b548b7c3c1d0
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-01-23 17:59:08 +0800

    ad9912: fix imports

--- artiq/coredevice/ad9912.py
+++ artiq/coredevice/ad9912.py
@@ -1,7 +1,7 @@
 from numpy import int32, int64
 
 from artiq.language.core import kernel, delay, portable
-from artiq.language.units import us, ns
+from artiq.language.units import ms, us, ns
 from artiq.coredevice.ad9912_reg import *
 
 from artiq.coredevice import spi2 as spi
