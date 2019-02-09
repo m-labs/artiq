a52234b5ff7b6824aa4f08b1b2fe72f847e7d181
Sebastien Bourdeauducq <sb@m-labs.hk>
2019-02-09 14:29:37 +0800

    nix: build firmware on hydra

--- nix/release.nix
+++ nix/release.nix
@@ -3,6 +3,7 @@ let
   artiqPkgs = import ./default.nix { inherit pkgs; };
   jobs = rec {
     conda-artiq = import ./conda-build.nix { inherit pkgs; };
+    artiq-board = import ./artiq-board.nix { inherit pkgs; };
   } // artiqPkgs;
 in
   jobs
