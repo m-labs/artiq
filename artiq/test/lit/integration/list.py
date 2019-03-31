baf102dbb23f13324ee74904fd8c35c45ab72774
David Nadlinger <code@klickverbot.at>
2019-03-31 02:14:53 +0100

    compiler: Fix comparison of nested lists

--- artiq/test/lit/integration/list.py
+++ artiq/test/lit/integration/list.py
@@ -7,3 +7,10 @@ assert (x, y) == (1, 2)
 
 lst = [1, 2, 3]
 assert [x*x for x in lst] == [1, 4, 9]
+
+assert [0] == [0]
+assert [0] != [1]
+assert [[0]] == [[0]]
+assert [[0]] != [[1]]
+assert [[[0]]] == [[[0]]]
+assert [[[0]]] != [[[1]]]
