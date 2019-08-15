dbec3b363772a86078281575f1b18a54ab495a0d
Stephan Maka <stephan@spaceboyz.net>
2019-08-16 01:13:35 +0200

    firmware: don't unwrap() but propagate pca9548 errors

--- artiq/firmware/libboard_artiq/pca9548.rs
+++ artiq/firmware/libboard_artiq/pca9548.rs
@@ -2,10 +2,10 @@ use i2c;
 
 pub fn select(busno: u8, address: u8, channels: u8) -> Result<(), &'static str> {
     i2c::start(busno).unwrap();
-    if !i2c::write(busno, address << 1).unwrap() {
+    if !i2c::write(busno, address << 1)? {
         return Err("PCA9548 failed to ack write address")
     }
-    if !i2c::write(busno, channels).unwrap() {
+    if !i2c::write(busno, channels)? {
         return Err("PCA9548 failed to ack control word")
     }
     i2c::stop(busno).unwrap();
