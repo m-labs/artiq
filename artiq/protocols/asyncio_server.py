ccf9b86917dd9abb72fb63b00e2de792eea2bfb6
Charles Baynham <charles.baynham@npl.co.uk>
2019-09-19 12:04:58 +0100

    asyncio_server: raise exceptions from tasks

--- artiq/protocols/asyncio_server.py
+++ artiq/protocols/asyncio_server.py
@@ -44,6 +44,8 @@ class AsyncioServer:
 
     def _client_done(self, task):
         self._client_tasks.remove(task)
+        if task.exception() and task.exception() != asyncio.CancelledError:
+            raise task.exception()
 
     def _handle_connection(self, reader, writer):
         task = asyncio.ensure_future(self._handle_connection_cr(reader, writer))
