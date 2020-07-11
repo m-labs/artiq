901be75ba4c08f3392ecc9398b0ebcead2bfc78c
Sebastien Bourdeauducq <sb@m-labs.hk>
2020-07-11 09:51:01 +0800

    sayma_rtm: fix Si5324 reset
    Closes #1483

--- artiq/gateware/targets/sayma_rtm.py
+++ artiq/gateware/targets/sayma_rtm.py
@@ -164,12 +164,13 @@ class _SatelliteBase(BaseSoC):
             platform.add_false_path_constraints(
                 self.crg.cd_sys.clk, self.siphaser.mmcm_freerun_output)
             self.csr_devices.append("siphaser")
+            self.submodules.si5324_rst_n = gpio.GPIOOut(platform.request("si5324").rst_n)
+            self.csr_devices.append("si5324_rst_n")
             i2c = self.platform.request("i2c")
             self.submodules.i2c = gpio.GPIOTristate([i2c.scl, i2c.sda])
             self.csr_devices.append("i2c")
             self.config["I2C_BUS_COUNT"] = 1
             self.config["HAS_SI5324"] = None
-            self.config["SI5324_SOFT_RESET"] = None
 
         platform.add_period_constraint(gtp.txoutclk, rtio_clk_period)
         platform.add_period_constraint(gtp.rxoutclk, rtio_clk_period)
