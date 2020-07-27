e82357d180582e3e392e7a009e70e361cdc49c68
David Nadlinger <code@klickverbot.at>
2020-07-27 23:19:43 +0100

    compiler: Fix inferencer tests after adding TArray.num_dims

--- artiq/test/lit/inferencer/array.py
+++ artiq/test/lit/inferencer/array.py
@@ -1,13 +1,13 @@
 # RUN: %python -m artiq.compiler.testbench.inferencer %s >%t
 # RUN: OutputCheck %s --file-to-check=%t
 
-# CHECK-L: numpy.array(elt='a)
+# CHECK-L: numpy.array(elt='a, num_dims=1)
 array([])
 
-# CHECK-L: numpy.array(elt=numpy.int?)
+# CHECK-L: numpy.array(elt=numpy.int?, num_dims=1)
 array([1, 2, 3])
-# CHECK-L: numpy.array(elt=numpy.int?)
+# CHECK-L: numpy.array(elt=numpy.int?, num_dims=2)
 array([[1, 2, 3], [4, 5, 6]])
 
-# CHECK-L: numpy.array(elt=list(elt=numpy.int?))
+# CHECK-L: numpy.array(elt=list(elt=numpy.int?), num_dims=1)
 array([[1, 2, 3], [4, 5]])
