aa04a081d3badf759d0ebf1ade86030b2e06795d
StarChen <sc@m-labs.hk>
2021-07-14 12:00:03 +0800

    applets: update `length_warning` slot for timer callback

--- artiq/applets/plot_hist.py
+++ artiq/applets/plot_hist.py
@@ -11,38 +11,40 @@ class HistogramPlot(pyqtgraph.PlotWidget):
     def __init__(self, args):
         pyqtgraph.PlotWidget.__init__(self)
         self.args = args
+        self.flag = True
         self.timer = QTimer()
-        self.timer.timeout.connect(self.warning)
+        self.timer.setSingleShot(True)
+        self.timer.timeout.connect(self.length_warning)
 
     def data_changed(self, data, mods, title):
         try:
-            self.y = data[self.args.y][1]
+            y = data[self.args.y][1]
             if self.args.x is None:
-                self.x = None
+                x = None
             else:
-                self.x = data[self.args.x][1]
+                x = data[self.args.x][1]
         except KeyError:
             return
-        if self.x is None:
-            self.x = list(range(len(self.y)+1))
+        if x is None:
+            x = list(range(len(y)+1))
 
-        if len(self.y) and len(self.x) == len(self.y) + 1:
+        if len(y) and len(x) == len(y) + 1:
+            self.flag = True
+            self.timer.stop()
             self.clear()
-            self.plot(self.x, self.y, stepMode=True, fillLevel=0,
+            self.plot(x, y, stepMode=True, fillLevel=0,
                       brush=(0, 0, 255, 150))
             self.setTitle(title)
         else:
-            self.timer.start(1000)
+            if self.flag:
+                self.flag = False
+                self.clear()
+                self.timer.start(1000)
 
-    def warning(self):
-        if len(self.y) and len(self.x) == len(self.y) + 1:
-            return
-        else:
-            self.timer.stop()
-            self.clear()
-            text = '''⚠️ dataset lengths mismatch:\n
-                      BIN_BOUNDARIES should be one more than COUNTS'''
-            self.addItem(pyqtgraph.TextItem(text))
+    def length_warning(self):
+        text = '''⚠️ dataset lengths mismatch:\n
+                    BIN_BOUNDARIES should be one more than COUNTS'''
+        self.addItem(pyqtgraph.TextItem(text))
 
 
 def main():
