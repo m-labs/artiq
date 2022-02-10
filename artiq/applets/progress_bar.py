7141975d5e06c9c7fb095e355e6081fdd24eaff9
Suthep Pomjaksilp <pomjaksi@physik.uni-kl.de>
2022-02-10 14:49:23 +0100

    Remove max dataset from QtProgressBar applet
    It was confusing and of minor usage. Now there is only one
    dataset value which is displayed between fixed min and max
    values.
    
    Signed-off-by: Suthep Pomjaksilp <pomjaksi@physik.uni-kl.de>

--- artiq/applets/progress_bar.py
+++ artiq/applets/progress_bar.py
@@ -8,30 +8,26 @@ from artiq.applets.simple import SimpleApplet
 class ProgressWidget(QtWidgets.QProgressBar):
     def __init__(self, args):
         QtWidgets.QProgressBar.__init__(self)
-        self.setMaximum(args.max)
         self.setMinimum(args.min)
-        self.dataset_n = args.n
-        self.dataset_n_max = args.n_max
+        self.setMaximum(args.max)
+        self.dataset_value = args.value
 
     def data_changed(self, data, mods):
         try:
-            n = int(round(data[self.dataset_n][1]))
-            n_max = int(round(data[self.dataset_n_max][1]))
+            value = round(data[self.dataset_value][1])
         except (KeyError, ValueError, TypeError):
-            n = 0
-            n_max = 0
-        self.setMaximum(n_max)
-        self.setValue(n)
+            value = 0
+        self.setValue(value)
+
 
 
 def main():
     applet = SimpleApplet(ProgressWidget)
-    applet.add_dataset("n", "counter")
-    applet.add_dataset("n_max", "maximimum counter")
+    applet.add_dataset("value", "counter")
     applet.argparser.add_argument("--min", type=int, default=0,
-                                  help="minimum")
+                                  help="minimum (left) value of the bar")
     applet.argparser.add_argument("--max", type=int, default=100,
-                                  help="maximum")
+                                  help="maximum (right) value of the bar")
     applet.run()
 
 if __name__ == "__main__":
