e76df491f244a15f12302b43899fb62b8d40e670
Sebastien Bourdeauducq <sb@m-labs.hk>
2022-02-14 16:39:26 +0800

    coredevice/exceptions: nac3 no longer breaks because of docstrings

--- artiq/coredevice/exceptions.py
+++ artiq/coredevice/exceptions.py
@@ -3,59 +3,59 @@ from artiq.language.core import nac3
 
 @nac3
 class RTIOUnderflow(Exception):
-    # NAC3TODO """Raised when the CPU or DMA core fails to submit a RTIO event early
-    # enough (with respect to the event's timestamp).
+    """Raised when the CPU or DMA core fails to submit a RTIO event early
+    enough (with respect to the event's timestamp).
 
-    # The offending event is discarded and the RTIO core keeps operating.
-    # """
+    The offending event is discarded and the RTIO core keeps operating.
+    """
     pass
 
 
 @nac3
 class RTIOOverflow(Exception):
-    # NAC3TODO """Raised when at least one event could not be registered into the RTIO
-    # input FIFO because it was full (CPU not reading fast enough).
+    """Raised when at least one event could not be registered into the RTIO
+    input FIFO because it was full (CPU not reading fast enough).
 
-    # This does not interrupt operations further than cancelling the current
-    # read attempt and discarding some events. Reading can be reattempted after
-    # the exception is caught, and events will be partially retrieved.
-    # """
+    This does not interrupt operations further than cancelling the current
+    read attempt and discarding some events. Reading can be reattempted after
+    the exception is caught, and events will be partially retrieved.
+    """
     pass
 
 
 @nac3
 class RTIODestinationUnreachable(Exception):
-    # NAC3TODO """Raised with a RTIO operation could not be completed due to a DRTIO link
-    # being down.
-    # """
+    """Raised with a RTIO operation could not be completed due to a DRTIO link
+    being down.
+    """
     pass
 
 
 @nac3
 class CacheError(Exception):
-    # NAC3TODO """Raised when putting a value into a cache row would violate memory safety."""
+    """Raised when putting a value into a cache row would violate memory safety."""
     pass
 
 
 @nac3
 class DMAError(Exception):
-    # NAC3TODO """Raised when performing an invalid DMA operation."""
+    """Raised when performing an invalid DMA operation."""
     pass
 
 
 @nac3
 class ClockFailure(Exception):
-    # NAC3TODO """Raised when RTIO PLL has lost lock."""
+    """Raised when RTIO PLL has lost lock."""
     pass
 
 
 @nac3
 class I2CError(Exception):
-    # NAC3TODO """Raised when a I2C transaction fails."""
+    """Raised when a I2C transaction fails."""
     pass
 
 
 @nac3
 class SPIError(Exception):
-    # NAC3TODO """Raised when a SPI transaction fails."""
+    """Raised when a SPI transaction fails."""
     pass
