130209fdbd465f3a691a04b53772d9246ff352a6
Deepskyhunter <ww@m-labs.hk>
2022-08-05 15:40:46 +0800

    ch_names: Print to stdout when no output file specified

--- artiq/frontend/artiq_ch_names.py
+++ artiq/frontend/artiq_ch_names.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import argparse
+import sys
 from artiq.master.databases import DeviceDB
 
 
@@ -8,21 +9,29 @@ def get_argparser():
                                      "template generator")
     parser.add_argument("-d", "--device-db", default="device_db.py",
                         help="device database file (default: '%(default)s')")
-    parser.add_argument("-o", "--output", default="channel_ntn.txt",
+    parser.add_argument("-o", "--output",
                         help="output file (default: '%(default)s')")
 
     return parser
 
 
+def process(output, ddb):
+    for device, value in ddb.items():
+        if "arguments" in value:
+            if "channel" in value["arguments"]:
+                print(":".join([str(value["arguments"]["channel"]),
+                                device]), file=output)
+
+
 def main():
     args = get_argparser().parse_args()
     ddb = DeviceDB(args.device_db).get_device_db()
-    with open(args.output, "w") as f:
-        for device, value in ddb.items():
-            if "arguments" in value:
-                if "channel" in value["arguments"]:
-                    print(":".join([str(value["arguments"]["channel"]),
-                                    device]), file=f)
+
+    if args.output is not None:
+        with open(args.output, "w") as f:
+            process(f, ddb)
+    else:
+        process(sys.stdout, ddb)
 
 
 if __name__ == "__main__":
