a6df1edd72fd95bcc386b412e9e63731a259e281
Deepskyhunter <ww@m-labs.hk>
2022-08-05 15:09:10 +0800

    test_scheduler: Put duplicated code to setUp() and tearDown()

--- artiq/test/test_scheduler.py
+++ artiq/test/test_scheduler.py
@@ -60,6 +60,7 @@ class _RIDCounter:
         self._next_rid += 1
         return rid
 
+
 class SchedulerMonitor:
     flow_map = {   # current status -> possible move
             "": {"pending"},
@@ -86,7 +87,8 @@ class SchedulerMonitor:
             if rid not in self.last_status.keys():
                 self.last_status[rid] = ""
             if exp_info["status"] != self.last_status[rid]:
-                self.test.assertIn(exp_info["status"], self.flow_map[self.last_status[rid]])
+                self.test.assertIn(exp_info["status"],
+                                   self.flow_map[self.last_status[rid]])
 
                 if rid in self.flags["arrive"].keys():
                     if exp_info["status"] in self.flags["arrive"][rid].keys():
@@ -107,6 +109,7 @@ class SchedulerMonitor:
             self.add_flag(rid, condition, status)
         await self.flags[condition][rid][status].wait()
         self.remove_flag(rid, condition, status)
+        return rid
 
     def add_flag(self, rid, condition, status):
         if rid not in self.flags[condition]:
@@ -125,34 +128,30 @@ class SchedulerCase(unittest.TestCase):
     def setUp(self):
         self.loop = asyncio.new_event_loop()
         asyncio.set_event_loop(self.loop)
+        self.handlers = {}
+        self.scheduler = Scheduler(_RIDCounter(0), self.handlers, None, None)
+        self.monitor = SchedulerMonitor(self)
+        self.scheduler.notifier.publish = self.monitor.record
+        self.scheduler.start()
 
     def test_steps(self):
-        loop = self.loop
-        scheduler = Scheduler(_RIDCounter(0), dict(), None, None)
         expid = _get_expid("EmptyExperiment")
-        monitor = SchedulerMonitor(self)
-        scheduler.notifier.publish = monitor.record
-        scheduler.start()
 
         # Verify that a timed experiment far in the future does not
         # get run, even if it has high priority.
         late = time() + 100000
-        scheduler.submit("main", expid, 99, late, False)
+        self.scheduler.submit("main", expid, 99, late, False)
 
         # This one (RID 1) gets run instead.
-        scheduler.submit("main", expid, 0, None, False)
+        self.scheduler.submit("main", expid, 0, None, False)
 
-        loop.run_until_complete(monitor.wait_until(1, "arrive", "deleting"))
-        self.assertEqual(monitor.last_status[0], "pending")
-        loop.run_until_complete(scheduler.stop())
+        self.loop.run_until_complete(self.monitor.wait_until(1, "arrive", "deleting"))
+        self.assertEqual(self.monitor.last_status[0], "pending")
 
     def test_pending_priority(self):
         """Check due dates take precedence over priorities when waiting to
         prepare."""
-        loop = self.loop
-        handlers = {}
-        scheduler = Scheduler(_RIDCounter(0), handlers, None, None)
-        handlers["scheduler_check_pause"] = scheduler.check_pause
+        self.handlers["scheduler_check_pause"] = self.scheduler.check_pause
 
         expid_empty = _get_expid("EmptyExperiment")
 
@@ -166,32 +165,26 @@ class SchedulerCase(unittest.TestCase):
         late = time() + 100000
         early = time() + 1
 
-        monitor = SchedulerMonitor(self)
-        scheduler.notifier.publish = monitor.record
-        scheduler.start()
+        self.scheduler.submit("main", expid_bg, low_priority)
+        self.scheduler.submit("main", expid_empty, high_priority, late)
+        self.scheduler.submit("main", expid_empty, middle_priority, early)
 
-        scheduler.submit("main", expid_bg, low_priority)
-        scheduler.submit("main", expid_empty, high_priority, late)
-        scheduler.submit("main", expid_empty, middle_priority, early)
-
-        wait_RID1_leave = loop.create_task(
-            monitor.wait_until(1, "leave", "pending"))
-        wait_RID2_leave = loop.create_task(
-            monitor.wait_until(2, "leave", "pending"))
-        done, pending = loop.run_until_complete(asyncio.wait(
+        wait_RID1_leave = self.loop.create_task(
+            self.monitor.wait_until(1, "leave", "pending"))
+        wait_RID2_leave = self.loop.create_task(
+            self.monitor.wait_until(2, "leave", "pending"))
+        done, pending = self.loop.run_until_complete(asyncio.wait(
             [wait_RID1_leave, wait_RID2_leave],
             return_when=asyncio.FIRST_COMPLETED))
         self.assertIn(wait_RID2_leave, done)
         for task in pending:
             task.cancel()
 
-        loop.run_until_complete(monitor.wait_until(2, "arrive", "deleting"))
-        loop.run_until_complete(scheduler.stop())
+        self.loop.run_until_complete(self.monitor.wait_until(2, "arrive", "deleting"))
 
     def test_pause(self):
-        loop = self.loop
-
         termination_ok = False
+
         def check_termination(mod):
             nonlocal termination_ok
             self.assertEqual(
@@ -199,90 +192,67 @@ class SchedulerCase(unittest.TestCase):
                 {"action": "setitem", "key": "termination_ok",
                  "value": (False, True), "path": []})
             termination_ok = True
-        handlers = {
-            "update_dataset": check_termination
-        }
-        scheduler = Scheduler(_RIDCounter(0), handlers, None, None)
+        self.handlers["update_dataset"] = check_termination
 
         expid_bg = _get_expid("BackgroundExperiment")
         expid = _get_expid("EmptyExperiment")
 
-        monitor = SchedulerMonitor(self)
-        scheduler.notifier.publish = monitor.record
-        scheduler.start()
         # check_pause is True when rid with higher priority is prepare_done
-        scheduler.submit("main", expid_bg, -99, None, False)
-        loop.run_until_complete(monitor.wait_until(0, "arrive", "running"))
-        self.assertFalse(scheduler.check_pause(0))
-        scheduler.submit("main", expid, 0, None, False)
-        self.assertFalse(scheduler.check_pause(0))
-        loop.run_until_complete(monitor.wait_until(1, "arrive", "prepare_done"))
-        self.assertTrue(scheduler.check_pause(0))
-        loop.run_until_complete(monitor.wait_until(1, "arrive", "deleting"))
-        self.assertFalse(scheduler.check_pause(0))
+        self.scheduler.submit("main", expid_bg, -99, None, False)
+        self.loop.run_until_complete(self.monitor.wait_until(0, "arrive", "running"))
+        self.assertFalse(self.scheduler.check_pause(0))
+        self.scheduler.submit("main", expid, 0, None, False)
+        self.assertFalse(self.scheduler.check_pause(0))
+        self.loop.run_until_complete(self.monitor.wait_until(1, "arrive", "prepare_done"))
+        self.assertTrue(self.scheduler.check_pause(0))
+        self.loop.run_until_complete(self.monitor.wait_until(1, "arrive", "deleting"))
+        self.assertFalse(self.scheduler.check_pause(0))
 
         # check_pause is True when request_termination is called
         self.assertFalse(termination_ok)
-        self.assertFalse(scheduler.check_pause(0))
-        scheduler.request_termination(0)
-        self.assertTrue(scheduler.check_pause(0))
-        loop.run_until_complete(monitor.wait_until(0, "arrive", "deleting"))
+        self.assertFalse(self.scheduler.check_pause(0))
+        self.scheduler.request_termination(0)
+        self.assertTrue(self.scheduler.check_pause(0))
+        self.loop.run_until_complete(self.monitor.wait_until(0, "arrive", "deleting"))
         self.assertTrue(termination_ok)
-
-        loop.run_until_complete(scheduler.stop())
 
     def test_close_with_active_runs(self):
         """Check scheduler exits with experiments still running"""
-        loop = self.loop
-
-        scheduler = Scheduler(_RIDCounter(0), {}, None, None)
-
         expid_bg = _get_expid("BackgroundExperiment")
         # Suppress the SystemExit backtrace when worker process is killed.
         expid_bg["log_level"] = logging.CRITICAL
-        monitor = SchedulerMonitor(self)
         expid = _get_expid("EmptyExperiment")
 
-        scheduler.notifier.publish = monitor.record
-        scheduler.start()
-        scheduler.submit("main", expid_bg, -99, None, False)
-        loop.run_until_complete(monitor.wait_until(0, "arrive", "running"))
+        self.scheduler.submit("main", expid_bg, -99, None, False)
+        self.loop.run_until_complete(self.monitor.wait_until(0, "arrive", "running"))
 
-        scheduler.submit("main", expid, 0, None, False)
-        loop.run_until_complete(monitor.wait_until(1, "arrive", "prepare_done"))
+        self.scheduler.submit("main", expid, 0, None, False)
+        self.loop.run_until_complete(self.monitor.wait_until(1, "arrive", "prepare_done"))
 
         # At this point, (at least) BackgroundExperiment is still running; make
         # sure we can stop the scheduler without hanging.
-        loop.run_until_complete(scheduler.stop())
 
     def test_flush(self):
-        loop = self.loop
-        handlers = {}
-        scheduler = Scheduler(_RIDCounter(0), handlers, None, None)
-        handlers["scheduler_check_pause"] = scheduler.check_pause
+        self.handlers["scheduler_check_pause"] = self.scheduler.check_pause
         expid = _get_expid("EmptyExperiment")
         expid_bg = _get_expid("CheckPauseBackgroundExperiment")
         expid_bg["log_level"] = logging.CRITICAL
-        monitor = SchedulerMonitor(self)
 
-        scheduler.notifier.publish = monitor.record
-        scheduler.start()
         # Flush with same priority
-        scheduler.submit("main", expid, 0, None, False)
-        scheduler.submit("main", expid, 0, None, True)
-        loop.run_until_complete(monitor.wait_until(1, "arrive", "preparing"))
-        self.assertEqual(monitor.last_status[0], "deleting")
-        loop.run_until_complete(monitor.wait_until(1, "arrive", "deleting"))
+        self.scheduler.submit("main", expid, 0, None, False)
+        self.scheduler.submit("main", expid, 0, None, True)
+        self.loop.run_until_complete(self.monitor.wait_until(1, "arrive", "preparing"))
+        self.assertEqual(self.monitor.last_status[0], "deleting")
+        self.loop.run_until_complete(self.monitor.wait_until(1, "arrive", "deleting"))
 
         # Flush with higher priority
-        scheduler.submit("main", expid_bg, 0, None, False)
+        self.scheduler.submit("main", expid_bg, 0, None, False)
         # Make sure RID 2 go into preparing stage first
-        loop.run_until_complete(monitor.wait_until(2, "arrive", "preparing"))
-        scheduler.submit("main", expid, 1, None, True)
-        loop.run_until_complete(monitor.wait_until(3, "arrive", "deleting"))
-        self.assertEqual(monitor.last_status[2], "running")
-
-        loop.run_until_complete(scheduler.stop())
+        self.loop.run_until_complete(self.monitor.wait_until(2, "arrive", "preparing"))
+        self.scheduler.submit("main", expid, 1, None, True)
+        self.loop.run_until_complete(self.monitor.wait_until(3, "arrive", "deleting"))
+        self.assertEqual(self.monitor.last_status[2], "running")
 
     def tearDown(self):
+        self.loop.run_until_complete(self.scheduler.stop())
         self.loop.close()
