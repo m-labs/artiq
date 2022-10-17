b9c171e7d1ea875ba13b7a6efd8606b407fe5349
yuk <yuk@m-labs.hk>
2022-10-17 16:21:45 +0800

    hdf5: Add demo for saving attributes
    Signed-off-by: yuk <yuk@m-labs.hk>

---
+++ artiq/examples/no_hardware/repository/hdf5_attributes.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+from artiq.experiment import *
+
+
+class HDF5Attributes(EnvExperiment):
+    """Archive data to HDF5 with attributes"""
+    def run(self):
+        # Attach attributes to the HDF5 group `datasets`
+        self.set_dataset_metadata(None, {
+            "arr": np.array([1, 2, 3]),
+            "description": "demo",
+        })
+
+        dummy = np.empty(20)
+        dummy.fill(np.nan)
+        # `archive=True` is required in order to
+        # attach attributes to HDF5 datasets
+        self.set_dataset("dummy", dummy,
+                         broadcast=True, archive=True)
+        self.set_dataset_metadata("dummy", {"k1": "v1", "k2": "v2"})
