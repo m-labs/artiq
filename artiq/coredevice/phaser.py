ab8bb9ef31390ed9feb39c82211f6887924bcde3
Fabian Schwartau <fabian@opencode.eu>
2022-10-19 15:45:45 +0200

    Fixed two too low delay values in Phaser init
    Signed-off-by: Fabian Schwartau <fabian@opencode.eu>

--- artiq/coredevice/phaser.py
+++ artiq/coredevice/phaser.py
@@ -237,7 +237,7 @@ class Phaser:
 
         for data in self.dac_mmap:
             self.dac_write(data >> 16, data)
-            delay(40*us)
+            delay(120*us)
         self.dac_sync()
         delay(40*us)
 
@@ -616,7 +616,7 @@ class Phaser:
         .. note:: Synchronising the NCO clears the phase-accumulator
         """
         config1f = self.dac_read(0x1f)
-        delay(.1*ms)
+        delay(.4*ms)
         self.dac_write(0x1f, config1f & ~int32(1 << 1))
         self.dac_write(0x1f, config1f | (1 << 1))
 
