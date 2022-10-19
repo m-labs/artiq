286f151d9a97d97d782dece6777f553b81e3c188
Sebastien Bourdeauducq <sb@m-labs.hk>
2022-10-19 13:05:51 +0800

    flake: switch to upstream llvmlite

--- flake.nix
+++ flake.nix
@@ -112,28 +112,6 @@
         '';
       };
 
-      llvmlite-new = pkgs.python3Packages.buildPythonPackage rec {
-        pname = "llvmlite";
-        version = "0.38.0";
-        src = pkgs.python3Packages.fetchPypi {
-          inherit pname;
-          version = "0.38.0";
-          sha256 = "qZ0WbM87EW87ntI7m3C6JBVkCpyXjzqqE/rUnFj0llw=";
-        };
-        nativeBuildInputs = [ pkgs.llvm_11 ];
-        # Disable static linking
-        # https://github.com/numba/llvmlite/issues/93
-        postPatch = ''
-          substituteInPlace ffi/Makefile.linux --replace "-static-libstdc++" ""
-          substituteInPlace llvmlite/tests/test_binding.py --replace "test_linux" "nope"
-        '';
-        # Set directory containing llvm-config binary
-        preConfigure = ''
-          export LLVM_CONFIG=${pkgs.llvm_11.dev}/bin/llvm-config
-        '';
-        doCheck = false;  # FIXME
-      };
-
       artiq-upstream = pkgs.python3Packages.buildPythonPackage rec {
         pname = "artiq";
         version = artiqVersion;
@@ -147,8 +125,8 @@
 
         nativeBuildInputs = [ pkgs.qt5.wrapQtAppsHook ];
         # keep llvm_x and lld_x in sync with llvmlite
-        propagatedBuildInputs = [ pkgs.llvm_11 pkgs.lld_11 llvmlite-new sipyco.packages.x86_64-linux.sipyco pythonparser artiq-comtools.packages.x86_64-linux.artiq-comtools ]
-          ++ (with pkgs.python3Packages; [ pyqtgraph pygit2 numpy dateutil scipy prettytable pyserial python-Levenshtein h5py pyqt5 qasync tqdm ]);
+        propagatedBuildInputs = [ pkgs.llvm_11 pkgs.lld_11 sipyco.packages.x86_64-linux.sipyco pythonparser artiq-comtools.packages.x86_64-linux.artiq-comtools ]
+          ++ (with pkgs.python3Packages; [ llvmlite pyqtgraph pygit2 numpy dateutil scipy prettytable pyserial python-Levenshtein h5py pyqt5 qasync tqdm ]);
 
         dontWrapQtApps = true;
         postFixup = ''
