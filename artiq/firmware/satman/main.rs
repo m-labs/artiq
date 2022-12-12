753c56ad47bb85f14a8ce7462eb33f646002773e
SingularitySurfer <Norman_Krackow@gmx.de>
2022-12-12 17:24:32 +0000

    implement pca9539 and runtime io-expander chip selection
    better comments and address translation
    
    fix spurious };
    
    unwrap init in runtime and return err instead of panic
    
    propagate error
    
    del unnecessary use
    
    Signed-off-by: SingularitySurfer <Norman_Krackow@gmx.de>

--- artiq/firmware/satman/main.rs
+++ artiq/firmware/satman/main.rs
@@ -488,8 +488,8 @@ pub extern fn main() -> i32 {
     let (mut io_expander0, mut io_expander1);
     #[cfg(all(soc_platform = "kasli", hw_rev = "v2.0"))]
     {
-        io_expander0 = board_misoc::io_expander::IoExpander::new(0);
-        io_expander1 = board_misoc::io_expander::IoExpander::new(1);
+        io_expander0 = board_misoc::io_expander::IoExpander::new(0).unwrap();
+        io_expander1 = board_misoc::io_expander::IoExpander::new(1).unwrap();
         io_expander0.init().expect("I2C I/O expander #0 initialization failed");
         io_expander1.init().expect("I2C I/O expander #1 initialization failed");
         #[cfg(has_wrpll)]
