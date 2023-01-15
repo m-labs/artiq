9e8167e1fac61aa6effb7d8ed31ce7f7168eac63
Sebastien Bourdeauducq <sb@m-labs.hk>
2023-01-15 12:26:52 +0800

    artiq_ddb_template: fix mistake in 18524911

--- artiq/frontend/artiq_ddb_template.py
+++ artiq/frontend/artiq_ddb_template.py
@@ -539,10 +539,10 @@ class PeripheralManager:
     def process_phaser(self, rtio_offset, peripheral):
         mode = peripheral.get("mode", "base")
         if mode == "miqro":
-            dac = f', "dac": {{"pll_m": 16, "pll_n": 3, "interpolation": 2}}, "gw_rev"={PHASER_GW_MIQRO}'
+            dac = f', "dac": {{"pll_m": 16, "pll_n": 3, "interpolation": 2}}, "gw_rev": {PHASER_GW_MIQRO}'
             n_channels = 3
         else:
-            dac = f', "gw_rev"={PHASER_GW_BASE}'
+            dac = f', "gw_rev": {PHASER_GW_BASE}'
             n_channels = 5
         self.gen("""
             device_db["{name}"] = {{
