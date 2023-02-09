eb9d1c9808a0b49fe8fa30a2826584e3fc9772dd
Egor Savkin <es@m-labs.hk>
2023-02-09 17:40:56 +0800

    Removed checks for error and RID
    Signed-off-by: Egor Savkin <es@m-labs.hk>

--- artiq/test/test_client.py
+++ artiq/test/test_client.py
@@ -4,8 +4,9 @@ import subprocess
 import sys
 import os
 import unittest
-import re
+import logging
 from tempfile import TemporaryDirectory
+import signal
 from pygit2 import init_repository, Signature
 
 EXPERIMENT_CONTENT = """
@@ -22,6 +23,12 @@ device_db = {}
 """
 
 
+def get_env():
+    env = os.environ.copy()
+    env["PYTHONUNBUFFERED"] = "1"
+    return env
+
+
 class TestClient(unittest.TestCase):
     def setUp(self):
         self.tmp_dir = TemporaryDirectory(prefix="test")
@@ -29,61 +36,48 @@ class TestClient(unittest.TestCase):
         self.exp_name = "experiment.py"
         self.exp_path = os.path.join(self.tmp_dir.name, self.exp_name)
         self.device_db_path = os.path.join(self.tmp_dir.name, "device_db.py")
-        self.env = os.environ.copy()
-        self.env["PYTHONUNBUFFERED"] = "1"
         with open(self.exp_path, "w") as f:
             f.write(EXPERIMENT_CONTENT)
         with open(self.device_db_path, "w") as f:
             f.write(DDB_CONTENT)
 
-    def await_master_output_line(self):
-        return self.master.stdout.readline().strip()
-
     def start_master(self, *args):
         self.master = subprocess.Popen([sys.executable, "-m", "artiq.frontend.artiq_master", "--device-db",
-                                        self.device_db_path, *args], encoding="utf8", env=self.env,
+                                        self.device_db_path, *args], encoding="utf8", env=get_env(),
                                        text=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        while self.await_master_output_line() != "ARTIQ master is now ready.":
+        while self.master.stdout.readline().strip() != "ARTIQ master is now ready.":
             pass
 
-    def wait_end_master_with_output(self):
-        first_line = self.await_master_output_line()
-        self.master.terminate()
-        # take only stdout output because stderr is piped to it
-        leftover_output = self.master.communicate()[0]
-        return first_line + leftover_output
+    def check_and_terminate_master(self):
+        while not ("test content" in self.master.stdout.readline()):
+            pass
+        if os.name == "nt":
+            try:
+                self.master.send_signal(signal.CTRL_C_EVENT)
+                self.master.wait()
+            except KeyboardInterrupt:
+                logging.warning("Master process haven't handled the CTRL_C_EVENT correctly")
+        else:
+            self.master.terminate()
+            self.master.wait()
+        self.master.stdout.close()
 
-    def run_client(self, *args):
-        return subprocess.run([sys.executable, "-m", "artiq.frontend.artiq_client", *args], check=True,
-                              capture_output=True, env=self.env, text=True, encoding="utf8").stdout.strip()
-
-    def check_experiment_run(self, client_output, master_output, script_name):
-        pattern_rid = re.compile(r"RID: (\d+)")
-        pattern_print = re.compile(r"INFO:worker\((\d+),{}\):print:test content"
-                                   .format(script_name.replace(".", "\\.")))
-        rid_run = int(re.findall(pattern_rid, client_output)[0])
-        rid_print = int(re.findall(pattern_print, master_output)[0])
-        self.assertEqual(rid_print, rid_run)
-        self.assertGreaterEqual(rid_print, 0)
-        self.assertNotIn("error", master_output.lower())
+    @staticmethod
+    def run_client(*args):
+        subprocess.run([sys.executable, "-m", "artiq.frontend.artiq_client", *args], check=True,
+                       capture_output=True, env=get_env(), text=True, encoding="utf8").check_returncode()
 
     def test_submit_outside_repo(self):
         self.start_master("-r", self.tmp_empty_dir.name)
-        client_output = self.run_client("submit", self.exp_path)
-        master_output = self.wait_end_master_with_output()
-        self.check_experiment_run(client_output, master_output, self.exp_name)
+        self.run_client("submit", self.exp_path)
 
     def test_submit_by_content(self):
         self.start_master("-r", self.tmp_empty_dir.name)
-        client_output = self.run_client("submit", self.exp_path, "--content")
-        master_output = self.wait_end_master_with_output()
-        self.check_experiment_run(client_output, master_output, "<none>")
+        self.run_client("submit", self.exp_path, "--content")
 
     def test_submit_by_file_repo(self):
         self.start_master("-r", self.tmp_dir.name)
-        client_output = self.run_client("submit", self.exp_name, "-R")
-        master_output = self.wait_end_master_with_output()
-        self.check_experiment_run(client_output, master_output, self.exp_name)
+        self.run_client("submit", self.exp_name, "-R")
 
     def test_submit_by_git_repo(self):
         repo = init_repository(self.tmp_dir.name)
@@ -95,10 +89,9 @@ class TestClient(unittest.TestCase):
         repo.create_commit("HEAD", signature, signature, commit_msg, tree, [])
 
         self.start_master("-r", self.tmp_dir.name, "-g")
-        client_output = self.run_client("submit", self.exp_name, "-R")
-        master_output = self.wait_end_master_with_output()
-        self.check_experiment_run(client_output, master_output, self.exp_name)
+        self.run_client("submit", self.exp_name, "-R")
 
     def tearDown(self):
+        self.check_and_terminate_master()
         self.tmp_dir.cleanup()
         self.tmp_empty_dir.cleanup()
