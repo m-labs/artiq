692ae22c5b04cad8f4026c5062528afcd64f0398
Egor Savkin <es@m-labs.hk>
2023-03-13 11:18:14 +0800

    Catch formatting exception instead of relying on hostness
    Also add tests for kernel exceptions
    
    Signed-off-by: Egor Savkin <es@m-labs.hk>

---
+++ artiq/test/coredevice/test_exceptions.py
@@ -0,0 +1,53 @@
+import re
+
+from artiq.experiment import *
+from artiq.test.hardware_testbench import ExperimentCase
+
+
+class CustomException(Exception):
+    pass
+
+
+class KernelFmtException(EnvExperiment):
+    def build(self):
+        self.setattr_device("core")
+
+    @kernel
+    def run(self):
+        self.throw()
+
+    def throw(self):
+        raise CustomException("{foo}")
+
+
+class KernelNestedFmtException(EnvExperiment):
+    def build(self):
+        self.setattr_device("core")
+
+    @kernel
+    def run(self):
+        try:
+            self.throw()
+        except:
+            raise RTIOUnderflow("{bar}")
+
+    def throw(self):
+        raise CustomException("{foo}")
+
+
+class TestExceptions(ExperimentCase):
+    def test_custom_formatted_kernel_exception(self):
+        with self.assertLogs() as captured:
+            with self.assertRaisesRegex(CustomException, r"CustomException\(\d+\): \{foo\}"):
+                self.execute(KernelFmtException)
+        self.assertEqual(captured.output, [
+            "ERROR:artiq.coredevice.comm_kernel:Couldn't format exception message `{foo}`: KeyError: 'foo'"])
+
+    def test_nested_formatted_kernel_exception(self):
+        with self.assertLogs() as captured:
+            with self.assertRaisesRegex(CustomException,
+                                        re.compile(r"CustomException\(\d+\): \{foo\}.*?RTIOUnderflow\(\d+\): \{bar\}",
+                                                   re.DOTALL)):
+                self.execute(KernelNestedFmtException)
+        self.assertEqual(captured.output, [
+            "ERROR:artiq.coredevice.comm_kernel:Couldn't format exception message `{foo}`: KeyError: 'foo'"])
