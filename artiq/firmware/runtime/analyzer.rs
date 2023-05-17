6c96033d41c85534aff2703100adc3704fb7ce04
mwojcik <mw@m-labs.hk>
2023-05-17 17:12:14 +0800

    analyzer: implement querying up satellites for data

--- artiq/firmware/runtime/analyzer.rs
+++ artiq/firmware/runtime/analyzer.rs
@@ -1,7 +1,11 @@
 use io::{Write, Error as IoError};
 use board_misoc::{csr, cache};
-use sched::{Io, TcpListener, TcpStream, Error as SchedError};
+use sched::{Io, Mutex, TcpListener, TcpStream, Error as SchedError};
 use analyzer_proto::*;
+use alloc::vec::Vec;
+use urc::Urc;
+use board_artiq::drtio_routing;
+use core::cell::RefCell;
 
 const BUFFER_SIZE: usize = 512 * 1024;
 
@@ -35,17 +39,78 @@ fn disarm() {
     }
 }
 
-fn worker(stream: &mut TcpStream) -> Result<(), IoError<SchedError>> {
-    let data = unsafe { &BUFFER.data[..] };
-    let overflow_occurred = unsafe { csr::rtio_analyzer::message_encoder_overflow_read() != 0 };
-    let total_byte_count = unsafe { csr::rtio_analyzer::dma_byte_count_read() };
-    let pointer = (total_byte_count % BUFFER_SIZE as u64) as usize;
-    let wraparound = total_byte_count >= BUFFER_SIZE as u64;
+#[cfg(has_drtio)]
+pub mod remote_analyzer {
+    use super::*;
+    use rtio_mgt::drtio;
+    
+    pub struct RemoteBuffer {
+        pub total_byte_count: u64,
+        pub sent_bytes: u32,
+        pub overflow_occurred: bool,
+        pub data: Vec<u8>
+    }
 
+    pub fn get_data(io: &Io, aux_mutex: &Mutex, ddma_mutex: &Mutex,
+        routing_table: &drtio_routing::RoutingTable,
+        up_destinations: &Urc<RefCell<[bool; drtio_routing::DEST_COUNT]>>) -> RemoteBuffer {
+            // gets data from satellites and returns consolidated data
+            let mut remote_data: Vec<u8> = Vec::new();
+            let mut remote_overflow = false;
+            let mut remote_sent_bytes = 0;
+            let mut remote_total_bytes = 0;
+
+            let data_vec = match drtio::analyzer_query(io, aux_mutex, ddma_mutex, routing_table, up_destinations) {
+                Ok(data_vec) => data_vec,
+                Err(e) => { error!("error retrieving remote analyzer data: {}", e); Vec::new() }
+            };
+            for data in data_vec {
+                remote_total_bytes += data.total_byte_count;
+                remote_sent_bytes += data.sent_bytes;
+                remote_overflow |= data.overflow_occurred;
+                remote_data.extend(data.data);
+            }
+
+            RemoteBuffer {
+                total_byte_count: remote_total_bytes,
+                sent_bytes: remote_sent_bytes,
+                overflow_occurred: remote_overflow,
+                data: remote_data
+            }
+        }
+}   
+
+
+
+fn worker(stream: &mut TcpStream, _io: &Io, _aux_mutex: &Mutex, _ddma_mutex: &Mutex,
+    _routing_table: &drtio_routing::RoutingTable,
+    _up_destinations: &Urc<RefCell<[bool; drtio_routing::DEST_COUNT]>>
+) -> Result<(), IoError<SchedError>> {
+    let local_data = unsafe { &BUFFER.data[..] };
+    let local_overflow_occurred = unsafe { csr::rtio_analyzer::message_encoder_overflow_read() != 0 };
+    let local_total_byte_count = unsafe { csr::rtio_analyzer::dma_byte_count_read() };
+
+    let wraparound = local_total_byte_count >= BUFFER_SIZE as u64;
+    let local_sent_bytes = if wraparound { BUFFER_SIZE as u32 } else { local_total_byte_count as u32 };
+    let pointer = (local_total_byte_count % BUFFER_SIZE as u64) as usize;
+
+    #[cfg(has_drtio)]
+    let remote = remote_analyzer::get_data(
+        _io, _aux_mutex, _ddma_mutex, _routing_table, _up_destinations);
+    #[cfg(has_drtio)]
     let header = Header {
-        total_byte_count: total_byte_count,
-        sent_bytes: if wraparound { BUFFER_SIZE as u32 } else { total_byte_count as u32 },
-        overflow_occurred: overflow_occurred,
+        total_byte_count: local_total_byte_count + remote.total_byte_count,
+        sent_bytes: local_sent_bytes + remote.sent_bytes,
+        overflow_occurred: local_overflow_occurred | remote.overflow_occurred,
+        log_channel: csr::CONFIG_RTIO_LOG_CHANNEL as u8,
+        dds_onehot_sel: true 
+    };
+
+    #[cfg(not(has_drtio))]
+    let header = Header {
+        total_byte_count: local_total_byte_count,
+        sent_bytes: local_sent_bytes,
+        overflow_occurred: local_overflow_occurred,
         log_channel: csr::CONFIG_RTIO_LOG_CHANNEL as u8,
         dds_onehot_sel: true  // kept for backward compatibility of analyzer dumps
     };
@@ -54,16 +119,20 @@ fn worker(stream: &mut TcpStream) -> Result<(), IoError<SchedError>> {
     stream.write_all("e".as_bytes())?;
     header.write_to(stream)?;
     if wraparound {
-        stream.write_all(&data[pointer..])?;
-        stream.write_all(&data[..pointer])?;
+        stream.write_all(&local_data[pointer..])?;
+        stream.write_all(&local_data[..pointer])?;
     } else {
-        stream.write_all(&data[..pointer])?;
+        stream.write_all(&local_data[..pointer])?;
     }
+    #[cfg(has_drtio)]
+    stream.write_all(&remote.data)?;
 
     Ok(())
 }
 
-pub fn thread(io: Io) {
+pub fn thread(io: Io, aux_mutex: &Mutex, ddma_mutex: &Mutex,
+    routing_table: &Urc<RefCell<drtio_routing::RoutingTable>>,
+    up_destinations: &Urc<RefCell<[bool; drtio_routing::DEST_COUNT]>>) {
     let listener = TcpListener::new(&io, 65535);
     listener.listen(1382).expect("analyzer: cannot listen");
 
@@ -75,7 +144,8 @@ pub fn thread(io: Io) {
 
         disarm();
 
-        match worker(&mut stream) {
+        let routing_table = routing_table.borrow();
+        match worker(&mut stream, &io, aux_mutex, ddma_mutex, &routing_table, up_destinations) {
             Ok(())   => (),
             Err(err) => error!("analyzer aborted: {}", err)
         }
