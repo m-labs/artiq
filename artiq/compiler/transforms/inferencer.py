105c21f5fb74d48acbb89c8b6b8a473fa3069e28
mwojcik <mw@m-labs.hk>
2023-08-21 17:04:24 +0800

    inferencer: don't count missing args for remote

--- artiq/compiler/transforms/inferencer.py
+++ artiq/compiler/transforms/inferencer.py
@@ -1454,7 +1454,7 @@ class Inferencer(algorithm.Visitor):
             passed_args[keyword.arg] = keyword.arg_loc
 
         for formalname in typ_args:
-            if formalname not in passed_args:
+            if formalname not in passed_args and not node.remote_args:
                 note = diagnostic.Diagnostic("note",
                     "the called function is of type {type}",
                     {"type": types.TypePrinter().name(node.func.type)},
