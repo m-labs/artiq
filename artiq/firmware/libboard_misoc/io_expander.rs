cd22e42cb4802d9fac9833ab747110340fc39644
linuswck <linuswck@m-labs.hk>
2023-08-23 06:21:14 +0000

    efc: add DRTIO virtual LEDs
    - EFC Gateware: Add virtual_leds to rtio
    - EFC Firmware: io_expander is kept being serviced to update
      virtual_leds after init

--- artiq/firmware/libboard_misoc/io_expander.rs
+++ artiq/firmware/libboard_misoc/io_expander.rs
@@ -83,7 +83,6 @@ impl IoExpander {
 
     #[cfg(soc_platform = "efc")]
     pub fn new() -> Result<Self, &'static str> {
-        // TODO: Put Virtual User LEDs L0 L1 in gateware
         const VIRTUAL_LED_MAPPING: [(u8, u8, u8); 2] = [(0, 0, 5), (1, 0, 6)];
 
         let mut io_expander = IoExpander {
