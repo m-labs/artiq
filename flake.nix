602b680b9da0da5fa1b49679cbb0acad1dd0b722
Sebastien Bourdeauducq <sb@m-labs.hk>
2023-09-14 10:57:37 +0800

    firmware: deal with rust nonsense
    Fixes
    "error: edition 2021 is unstable and only available with -Z unstable-options.
    error: could not compile `alloc`"

--- flake.nix
+++ flake.nix
@@ -42,6 +42,10 @@
         cargo = rust;
       });
 
+      cargo-xbuild = pkgs.cargo-xbuild.overrideAttrs(oa: {
+        postPatch = "substituteInPlace src/sysroot.rs --replace 2021 2018";
+      });
+
       vivadoDeps = pkgs: with pkgs; [
         libxcrypt-legacy
         ncurses5
@@ -186,7 +190,7 @@
           nativeBuildInputs = [
             (pkgs.python3.withPackages(ps: [ migen misoc (artiq.withExperimentalFeatures experimentalFeatures) ps.packaging ]))
             rust
-            pkgs.cargo-xbuild
+            cargo-xbuild
             pkgs.llvmPackages_14.clang-unwrapped
             pkgs.llvm_14
             pkgs.lld_14
@@ -360,7 +364,7 @@
         buildInputs = [
           (packages.x86_64-linux.python3-mimalloc.withPackages(ps: with packages.x86_64-linux; [ migen misoc ps.paramiko microscope ps.packaging ] ++ artiq.propagatedBuildInputs))
           rust
-          pkgs.cargo-xbuild
+          cargo-xbuild
           pkgs.llvmPackages_14.clang-unwrapped
           pkgs.llvm_14
           pkgs.lld_14
@@ -383,7 +387,7 @@
         buildInputs = [
           (pkgs.python3.withPackages(ps: with packages.x86_64-linux; [ migen misoc artiq ps.packaging ]))
           rust
-          pkgs.cargo-xbuild
+          cargo-xbuild
           pkgs.llvmPackages_14.clang-unwrapped
           pkgs.llvm_14
           pkgs.lld_14
