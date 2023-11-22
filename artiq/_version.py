56418e342e2779b3e5a191c96d5d96c162ddcb4e
Sebastien Bourdeauducq <sb@m-labs.hk>
2023-11-22 20:51:02 +0800

    take into account VERSIONEER_REV in artiq._version.get_rev

--- artiq/_version.py
+++ artiq/_version.py
@@ -1,7 +1,7 @@
 import os
 
 def get_rev():
-    return "unknown"
+    return os.getenv("VERSIONEER_REV", default="unknown")
 
 def get_version():
     return os.getenv("VERSIONEER_OVERRIDE", default="8.0+unknown.beta")
