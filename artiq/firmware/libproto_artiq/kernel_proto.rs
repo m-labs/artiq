d79462fa2dccff85e8d01d7efd20341cbc32dcf7
mwojcik <mw@m-labs.hk>
2024-01-31 16:48:48 +0800

    subkernel: fix passing arguments

--- artiq/firmware/libproto_artiq/kernel_proto.rs
+++ artiq/firmware/libproto_artiq/kernel_proto.rs
@@ -108,7 +108,7 @@ pub enum Message<'a> {
     SubkernelAwaitFinishRequest { id: u32, timeout: i64 },
     SubkernelAwaitFinishReply { status: SubkernelStatus },
     SubkernelMsgSend { id: u32, destination: Option<u8>, count: u8, tag: &'a [u8], data: *const *const () },
-    SubkernelMsgRecvRequest { id: u32, timeout: i64, tags: &'a [u8] },
+    SubkernelMsgRecvRequest { id: i32, timeout: i64, tags: &'a [u8] },
     SubkernelMsgRecvReply { status: SubkernelStatus, count: u8 },
 
     Log(fmt::Arguments<'a>),
