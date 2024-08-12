dcfc9e114ab53c980cf88ed73c84e9562d03b6aa
abdul124 <ar@m-labs.hk>
2024-08-12 17:31:43 +0800

    firmware/ksupport: improve comments and syscall name

--- artiq/compiler/embedding.py
+++ artiq/compiler/embedding.py
@@ -57,9 +57,9 @@ class EmbeddingMap:
         self.str_forward_map = {}
         self.str_reverse_map = {}
 
-        # Keep this list of exceptions in sync with `EXCEPTION_ID_LOOKUP` in `artiq.firmware.ksupport.eh_artiq``
-        # The exceptions declared here should be defined in `artiq.coredeive.exceptions``
-        # Without sync, test cases in artiq.test.coredevice.test_exceptions would fail
+        # Keep this list of exceptions in sync with `EXCEPTION_ID_LOOKUP` in `artiq::firmware::ksupport::eh_artiq`
+        # The exceptions declared here must be defined in `artiq.coredevice.exceptions`
+        # Verify synchronization by running the test cases in `artiq.test.coredevice.test_exceptions`
         self.preallocate_runtime_exception_names([
             "RTIOUnderflow",
             "RTIOOverflow",
