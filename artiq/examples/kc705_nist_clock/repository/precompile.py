039411f162d115be665ed01d129c158d290feb0d
spaqin <mw@m-labs.hk>
2024-08-13 09:28:02 +0200

    examples: fix precompile example for NAC3

--- artiq/examples/kc705_nist_clock/repository/precompile.py
+++ artiq/examples/kc705_nist_clock/repository/precompile.py
@@ -1,20 +1,21 @@
 from artiq.experiment import *
 
 
-# NAC3TODO
-
+@nac3
 class Precompile(EnvExperiment):
+    hello_str: Kernel[str]
+
     def build(self):
         self.setattr_device("core")
         self.hello_str = "hello ARTIQ"
 
     def prepare(self):
-    	self.precompiled = self.core.precompile(self.hello, "world")
+        self.precompiled = self.core.precompile(self.hello, "world")
 
     @kernel
-    def hello(self, arg):
-    	print(self.hello_str, arg)
-    	self.hello_str = "nowriteback"
+    def hello(self, arg: str):
+        print_rpc((self.hello_str, arg))
+        self.hello_str = "nowriteback"
 
     def run(self):
         self.precompiled()
