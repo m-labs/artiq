e389d60ae51c3b805820276fb026279ec77fe944
abdul124 <ar@m-labs.hk>
2024-08-19 11:43:26 +0800

    sync exception names and ids

--- artiq/compiler/embedding.py
+++ artiq/compiler/embedding.py
@@ -57,16 +57,31 @@ class EmbeddingMap:
         self.str_forward_map = {}
         self.str_reverse_map = {}
 
-        self.preallocate_runtime_exception_names(["RuntimeError",
-                                                  "RTIOUnderflow",
-                                                  "RTIOOverflow",
-                                                  "RTIODestinationUnreachable",
-                                                  "DMAError",
-                                                  "I2CError",
-                                                  "CacheError",
-                                                  "SPIError",
-                                                  "0:ZeroDivisionError",
-                                                  "0:IndexError"])
+        # Keep this list of exceptions in sync with `EXCEPTION_ID_LOOKUP` in `artiq.firmware.ksupport.eh_artiq``
+        # The exceptions declared here should be defined in `artiq.coredeive.exceptions``
+        # Without sync, test cases in artiq.test.coredevice.test_exceptions would fail
+        self.preallocate_runtime_exception_names([
+            "RTIOUnderflow",
+            "RTIOOverflow",
+            "RTIODestinationUnreachable",
+            "DMAError",
+            "I2CError",
+            "CacheError",
+            "SPIError",
+
+            "0:AssertionError",
+            "0:AttributeError",
+            "0:IndexError",
+            "0:IOError",
+            "0:KeyError",
+            "0:NotImplementedError",
+            "0:OverflowError",
+            "0:RuntimeError",
+            "0:TimeoutError",
+            "0:TypeError",
+            "0:ValueError",
+            "0:ZeroDivisionError"
+        ])
 
     def preallocate_runtime_exception_names(self, names):
         for i, name in enumerate(names):
