39d139658e09f66a5104d52e3f72d3c6c308e705
Florian Agbuya <fa@m-labs.ph>
2024-08-27 13:21:44 +0800

    update default version

--- artiq/_version.py
+++ artiq/_version.py
@@ -4,4 +4,4 @@ def get_rev():
     return os.getenv("VERSIONEER_REV", default="unknown")
 
 def get_version():
-    return os.getenv("VERSIONEER_OVERRIDE", default="8.0+unknown.beta")
+    return os.getenv("VERSIONEER_OVERRIDE", default="9.0+unknown.beta")
