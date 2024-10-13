f9cc4ff4eaab07487ed7b96e3e7ca44446e31c46
newell <newell.jensen@gmail.com>
2024-10-13 16:28:38 -0700

    Initial commit

---
+++ artiq/coredevice/ad9834.py
@@ -0,0 +1,353 @@
+"""
+RTIO Driver for the Analog Devices AD9834 DDS via 3-wire SPI interface.
+"""
+
+# https://github.com/analogdevicesinc/linux/blob/main/drivers/staging/iio/frequency/ad9834.c
+# https://www.analog.com/media/en/technical-documentation/data-sheets/AD9834.pdf
+# https://www.analog.com/media/en/technical-documentation/app-notes/an-1070.pdf
+
+from enum import Enum
+
+from artiq.experiment import *
+from artiq.language.core import *
+from artiq.language.types import *
+from artiq.language.units import *
+from artiq.coredevice import spi2 as spi
+
+from artiq.coredevice.spi2 import SPIMaster
+from artiq.coredevice.core import Core
+
+# Control Register Bits
+
+AD9834_B28 = 1 << 13
+AD9834_HLB = 1 << 12
+AD9834_FSEL = 1 << 11
+AD9834_PSEL = 1 << 10
+AD9834_PIN_SW = 1 << 9
+AD9834_RESET = 1 << 8
+AD9834_SLEEP1 = 1 << 7
+AD9834_SLEEP12 = 1 << 6
+AD9834_OPBITEN = 1 << 5
+AD9834_SIGN_PIB = 1 << 4
+AD9834_DIV2 = 1 << 3
+AD9834_MODE = 1 << 1
+
+
+class FreqReg(Enum):
+    REG0 = 0b01 << 14
+    REG1 = 0b10 << 14
+
+
+class PhaseReg(Enum):
+    REG0 = (0b11 << 14) | (0 << 13)
+    REG1 = (0b11 << 14) | (1 << 13)
+
+
+class AD9834:
+    """
+    AD9834 DDS channel driver.
+
+    This class provides control for a single DDS channel on the AD9834.
+
+    The driver utilizes bit-controlled FSELECT, PSELECT, and RESET.
+    To pin control these, set AD9834_PIN_SW.
+    The `ctrl_reg` attribute is used to maintain the state of the control register,
+    enabling persistent management of various configurations.
+
+    :param spi_device: SPI bus device name.
+    :param spi_freq: SPI bus clock frequency (default: 10 MHz, max: 40 MHz).
+    :param clk_freq: DDS clock frequency (default: 75 MHz).
+    :param core_device: Core device name (default: "core").
+    """
+
+    kernel_invariants = {"core", "bus", "spi_freq", "clk_freq"}
+
+    def __init__(
+        self, dmgr, spi_device, spi_freq=10 * MHz, clk_freq=75 * MHz, core_device="core"
+    ):
+        self.core = dmgr.get(core_device)
+        self.bus = dmgr.get(spi_device)
+        self.spi_freq = spi_freq
+        self.clk_freq = clk_freq
+        self.ctrl_reg = 0x0000
+        assert (
+            self.spi_freq <= 40 * MHz
+        ), "SPI frequency exceeds maximum value of 40 MHz"
+
+    @kernel
+    def init(self):
+        """
+        Initialize the AD9834, configure the SPI bus, and reset the DDS.
+
+        This method performs the necessary setup for the AD9834 device, including:
+        - Configuring the SPI bus parameters (clock polarity, data width, and frequency).
+        - Putting the AD9834 into a reset state to ensure proper initialization.
+
+        The SPI bus is configured to use 16 bits of data width with the clock frequency
+        provided as a parameter when creating the `AD9834` instance. After configuring
+        the SPI bus, the method invokes the `enable_reset` method to reset the AD9834.
+        This is an essential step to prepare the device for subsequent configuration
+        of frequency and phase.
+
+        This method should be called before any other operations are performed
+        on the AD9834 to ensure that the device is in a known state.
+        """
+        self.bus.set_config(spi.SPI_CLK_POLARITY | spi.SPI_END, 16, self.spi_freq, 1)
+        self.enable_reset()
+
+    @kernel
+    def set_frequency(self, freq_reg: FreqReg, frequency: TInt32):
+        """
+        Set the frequency for the specified FREQ register.
+
+        This method calculates the frequency word based on the provided frequency in Hz
+        and writes it to the specified frequency register.
+
+        :param freq_reg: The frequency register to write to, must be one of
+                         FreqReg.REG0 or FreqReg.REG1.
+        :param frequency: The desired frequency in Hz, which will be converted to a
+                         frequency word suitable for the AD9834.
+
+        The frequency word is calculated using the formula:
+            freq_word = (frequency * (1 << 28)) / clk_freq
+        The result is limited to the lower 28 bits for compatibility with the AD9834.
+
+        The method first sets the control register to enable the appropriate settings,
+        then sends the least significant byte (LSB) and most significant byte (MSB) in
+        two consecutive writes to the specified frequency register.
+        """
+        freq_word = int((frequency * (1 << 28)) / self.clk_freq) & 0x0FFFFFFF
+        self.ctrl_reg |= AD9834_B28
+        self.write(self.ctrl_reg)
+        lsb = freq_word & 0x3FFF
+        msb = (freq_word >> 14) & 0x3FFF
+        self.write(freq_reg.value | lsb)
+        self.write(freq_reg.value | msb)
+
+    @kernel
+    def set_frequency_msb_word(self, freq_reg: FreqReg, word: TInt32):
+        """
+        Set the most significant byte (MSB) of the specified FREQ register.
+
+        This method updates the specified frequency register with the provided MSB value.
+        It configures the control register to indicate that the MSB is being set.
+
+        :param freq_reg: The frequency register to update, must be one of
+                         FreqReg.REG0 or FreqReg.REG1.
+        :param word: The MSB value to be written to the frequency register,
+                     limited to the lower 14 bits.
+
+        The method first clears the appropriate control bits, sets the HLB bit to
+        indicate that the MSB is being sent, and then writes the updated control register
+        followed by the MSB value to the specified frequency register.
+        """
+        self.ctrl_reg &= ~AD9834_B28
+        self.ctrl_reg |= AD9834_HLB
+        self.write(self.ctrl_reg)
+        self.write(freq_reg.value | (word & 0x3FFF))
+
+    @kernel
+    def set_frequency_lsb_word(self, freq_reg: FreqReg, word: TInt32):
+        """
+        Set the least significant byte (LSB) of the specified FREQ register.
+
+        This method updates the specified frequency register with the provided LSB value.
+        It configures the control register to indicate that the LSB is being set.
+
+        :param freq_reg: The frequency register to update, must be one of
+                         FreqReg.REG0 or FreqReg.REG1.
+        :param word: The LSB value to be written to the frequency register,
+                     limited to the lower 14 bits.
+
+        The method first clears the appropriate control bits and writes the updated control
+        register followed by the LSB value to the specified frequency register.
+        """
+        self.ctrl_reg &= ~AD9834_B28
+        self.ctrl_reg &= ~AD9834_HLB
+        self.write(self.ctrl_reg)
+        self.write(freq_reg.value | (word & 0x3FFF))
+
+    @kernel
+    def set_phase(self, phase_reg: PhaseReg, phase: TInt32):
+        """
+        Set the phase for the specified PHASE register.
+
+        This method updates the specified phase register with the provided phase value.
+
+        :param phase_reg: The phase register to update, must be one of
+                          PhaseReg.REG0 or PhaseReg.REG1.
+        :param phase: The phase value to be written to the phase register,
+                      limited to the lower 12 bits.
+
+        The method masks the phase value to ensure it fits within the 12-bit limit
+        and writes it to the specified phase register.
+        """
+        phase_word = phase & 0x0FFF
+        self.write(phase_reg.value | phase_word)
+
+    @kernel
+    def enable_reset(self):
+        """
+        Enable the DDS reset.
+
+        This method sets the reset bit in the control register, putting the AD9834
+        into a reset state. While in this state, the digital-to-analog converter
+        (DAC) is not operational.
+
+        This method should be called during initialization or when a reset is required
+        to reinitialize the device and ensure proper operation.
+        """
+        self.ctrl_reg |= AD9834_RESET
+        self.write(self.ctrl_reg)
+
+    @kernel
+    def output_enable(self):
+        """
+        Disable the DDS reset and start signal generation.
+
+        This method clears the reset bit in the control register, allowing the AD9834
+        to begin generating signals. Once this method is called, the device will
+        resume normal operation and output the generated waveform.
+
+        This method should be called after configuration of the frequency and phase
+        settings to activate the output.
+        """
+        self.ctrl_reg &= ~AD9834_RESET
+        self.write(self.ctrl_reg)
+
+    @kernel
+    def sleep(
+        self,
+        dac_powerdown: bool = False,
+        internal_clk_disable: bool = False,
+        pin_sw: bool = False,
+    ):
+        """
+        Put the AD9834 into sleep mode based on specified conditions.
+
+        :param dac_powerdown: Power down the DAC (SLEEP12 bit).
+        :param internal_clk_disable: Disable the internal clock (SLEEP1 bit).
+        :param pin_sw: Control via PIN/SW (PIN/SW bit).
+        """
+        # Control PIN/SW bit
+        if pin_sw:
+            self.ctrl_reg |= AD9834_PIN_SW
+        else:
+            self.ctrl_reg &= ~AD9834_PIN_SW
+
+        # Handle SLEEP12 (DAC power-down)
+        if dac_powerdown:
+            self.ctrl_reg |= AD9834_SLEEP12
+        else:
+            self.ctrl_reg &= ~AD9834_SLEEP12
+
+        # Handle SLEEP1 (internal clock disable)
+        if internal_clk_disable:
+            self.ctrl_reg |= AD9834_SLEEP1
+        else:
+            self.ctrl_reg &= ~AD9834_SLEEP1
+
+        # Write the updated control register to the AD9834
+        self.write(self.ctrl_reg)
+
+    @kernel
+    def configure_sign_bit_out(
+        self,
+        high_impedance: bool = False,
+        dac_data_msb_2: bool = False,
+        dac_data_msb: bool = False,
+        comparator_output: bool = False,
+    ):
+        """
+        Configure the SIGN BIT OUT pin based on boolean flags.
+
+        The user can pass True for one of the parameters to set the desired output mode.
+
+        :param high_impedance: Set to True for High Impedance mode.
+        :param dac_data_msb_2: Set to True for DAC Data MSB/2 output.
+        :param dac_data_msb: Set to True for DAC Data MSB output.
+        :param comparator_output: Set to True for Comparator output.
+        """
+
+        # Default to high impedance if nothing is explicitly enabled
+        if high_impedance:
+            # High impedance state, clear OPBITEN
+            self.ctrl_reg &= ~AD9834_OPBITEN
+
+        elif dac_data_msb_2:
+            # DAC data MSB/2: OPBITEN = 1, MODE = 0, SIGN/PIB = 0, DIV2 = 0
+            self.ctrl_reg |= AD9834_OPBITEN
+            self.ctrl_reg &= ~AD9834_MODE
+            self.ctrl_reg &= ~AD9834_SIGN_PIB
+            self.ctrl_reg &= ~AD9834_DIV2
+
+        elif dac_data_msb:
+            # DAC data MSB: OPBITEN = 1, MODE = 0, SIGN/PIB = 0, DIV2 = 1
+            self.ctrl_reg |= AD9834_OPBITEN
+            self.ctrl_reg &= ~AD9834_MODE
+            self.ctrl_reg &= ~AD9834_SIGN_PIB
+            self.ctrl_reg |= AD9834_DIV2
+
+        elif comparator_output:
+            # Comparator output: OPBITEN = 1, MODE = 0, SIGN/PIB = 1, DIV2 = 1
+            self.ctrl_reg |= AD9834_OPBITEN
+            self.ctrl_reg &= ~AD9834_MODE
+            self.ctrl_reg |= AD9834_SIGN_PIB
+            self.ctrl_reg |= AD9834_DIV2
+
+        else:
+            # Default to high impedance if no valid mode is selected
+            self.ctrl_reg &= ~AD9834_OPBITEN
+
+        # Write the updated control register to the AD9834
+        self.write(self.ctrl_reg)
+
+    @kernel
+    def enable_triangular_waveform(self):
+        """
+        Enable triangular waveform generation.
+
+        This method configures the AD9834 to output a triangular waveform. It does so
+        by clearing the OPBITEN bit in the control register and setting the MODE bit.
+        Once this method is called, the AD9834 will begin generating a triangular waveform
+        at the frequency set for the selected frequency register.
+
+        This method should be called when a triangular waveform is desired for signal
+        generation. Ensure that the frequency is set appropriately before invoking this method.
+        """
+        self.ctrl_reg &= ~AD9834_OPBITEN
+        self.ctrl_reg |= AD9834_MODE
+        self.write(self.ctrl_reg)
+
+    @kernel
+    def disable_triangular_waveform(self):
+        """
+        Disable triangular waveform generation.
+
+        This method disables the triangular waveform output by clearing the MODE bit
+        in the control register. After invoking this method, the AD9834 will cease
+        generating a triangular waveform. The device can then be configured to output
+        other waveform types if needed.
+
+        This method should be called when switching to a different waveform type or
+        when the triangular waveform is no longer required.
+        """
+        self.ctrl_reg &= ~AD9834_MODE
+        self.write(self.ctrl_reg)
+
+    @kernel
+    def write(self, data: TInt32):
+        """
+        Write a 16-bit word to the AD9834.
+
+        This method sends a 16-bit data word to the AD9834 via the SPI bus. The input
+        data is left-shifted by 16 bits to ensure proper alignment for the SPI controller,
+        allowing for accurate processing of the command by the AD9834.
+
+        This method is used internally by other methods to update the control registers
+        and frequency settings of the AD9834. It should not be called directly unless
+        low-level register manipulation is required.
+
+        :param data: The 16-bit word to be sent to the AD9834.
+        """
+        self.bus.write(data << 16)
