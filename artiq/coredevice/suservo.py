4bc5d1baf70af2f0433f1c135807e9bba5dbb3ce
newell <newell.jensen@gmail.com>
2024-10-17 09:37:53 -0700

    Fix some typos

--- artiq/coredevice/suservo.py
+++ artiq/coredevice/suservo.py
@@ -479,7 +479,7 @@ class Channel:
     def get_y_mu(self, profile):
         """Get a profile's IIR state (filter output, Y0) in machine units.
 
-        The IIR state is also know as the "integrator", or the DDS amplitude
+        The IIR state is also known as the "integrator", or the DDS amplitude
         scale factor. It is 17 bits wide and unsigned.
 
         This method does not advance the timeline but consumes all slack.
@@ -497,7 +497,7 @@ class Channel:
     def get_y(self, profile):
         """Get a profile's IIR state (filter output, Y0).
 
-        The IIR state is also know as the "integrator", or the DDS amplitude
+        The IIR state is also known as the "integrator", or the DDS amplitude
         scale factor. It is 17 bits wide and unsigned.
 
         This method does not advance the timeline but consumes all slack.
@@ -515,7 +515,7 @@ class Channel:
     def set_y_mu(self, profile, y):
         """Set a profile's IIR state (filter output, Y0) in machine units.
 
-        The IIR state is also know as the "integrator", or the DDS amplitude
+        The IIR state is also known as the "integrator", or the DDS amplitude
         scale factor. It is 17 bits wide and unsigned.
 
         This method must not be used when the servo could be writing to the
@@ -535,7 +535,7 @@ class Channel:
     def set_y(self, profile, y):
         """Set a profile's IIR state (filter output, Y0).
 
-        The IIR state is also know as the "integrator", or the DDS amplitude
+        The IIR state is also known as the "integrator", or the DDS amplitude
         scale factor. It is 17 bits wide and unsigned.
 
         This method must not be used when the servo could be writing to the
