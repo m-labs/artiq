83cf0112491166459ee3e05682064e2455f9965f
Felix Vietmeyer <Felix.Vietmeyer@JILA.colorado.edu>
2024-12-06 19:09:36 -0500

    keep generic efc builds and shuttler specific builds separate

--- artiq/firmware/satman/main.rs
+++ artiq/firmware/satman/main.rs
@@ -26,7 +26,7 @@ use board_artiq::si549;
 use board_misoc::irq;
 use board_misoc::{boot, spiflash};
 use board_artiq::{spi, drtioaux, drtio_routing};
-#[cfg(soc_platform = "efc")]
+#[cfg(all(soc_platform = "efc", has_converter_spi))]
 use board_artiq::ad9117;
 use proto_artiq::drtioaux_proto::{SAT_PAYLOAD_MAX_SIZE, MASTER_PAYLOAD_MAX_SIZE, CXP_PAYLOAD_MAX_SIZE};
 #[cfg(has_drtio_eem)]
@@ -1025,7 +1025,7 @@ fn startup() {
 
     let mut hardware_tick_ts = 0;
 
-    #[cfg(soc_platform = "efc")]
+    #[cfg(all(soc_platform = "efc", has_converter_spi))]
     ad9117::init().expect("AD9117 initialization failed");
     
     loop {
