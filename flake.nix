d49ef555d1300cc84ebfbc8de11e41fbca4c1328
Sebastien Bourdeauducq <sb@m-labs.hk>
2025-01-15 11:53:47 +0800

    flake: switch to Vivado 2024.2

--- flake.nix
+++ flake.nix
@@ -269,7 +269,7 @@
     vivado = pkgs.buildFHSEnv {
       name = "vivado";
       targetPkgs = vivadoDeps;
-      profile = "set -e; source /opt/Xilinx/Vivado/2022.2/settings64.sh";
+      profile = "set -e; source /opt/Xilinx/Vivado/2024.2/settings64.sh";
       runScript = "vivado";
     };
 
