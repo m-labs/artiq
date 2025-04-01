256c12900699de3ad87fbbbc1d089d2c48aff365
occheung <dc@m-labs.hk>
2025-04-01 17:20:32 +0800

    fix LLVM string codegen test
    See #2713 for the main changes.

--- artiq/test/lit/embedding/syscall_arg_attrs.py
+++ artiq/test/lit/embedding/syscall_arg_attrs.py
@@ -4,21 +4,21 @@
 from artiq.language.core import *
 from artiq.language.types import *
 
-# Make sure `byval` and `sret` are specified both at the call site and the
+# Make sure `sret` is specified both at the call site and the
 # declaration. This isn't caught by the LLVM IR validator, but mismatches
 # lead to miscompilations (at least in LLVM 11).
 
 
 @kernel
 def entrypoint():
-    # CHECK: call void @accept_str\({ i8\*, i32 }\* nonnull byval
+    # CHECK: call void @accept_str\({ i8\*, i32 }
     accept_str("foo")
 
     # CHECK: call void @return_str\({ i8\*, i32 }\* nonnull sret
     return_str()
 
 
-# CHECK: declare void @accept_str\({ i8\*, i32 }\* byval\({ i8\*, i32 }\)\)
+# CHECK: declare void @accept_str\({ i8\*, i32 }\)
 @syscall
 def accept_str(name: TStr) -> TNone:
     pass
