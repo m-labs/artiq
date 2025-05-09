e39af04cfe4f3107927f5c45afe3e3807a3963c8
Simon Renblad <srenblad@m-labs.hk>
2025-05-09 11:18:19 +0800

    flake: move to naersk, remove dev-dependencies hack

--- flake.nix
+++ flake.nix
@@ -9,6 +9,11 @@
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
+    naersk = {
+      url = "github:nix-community/naersk";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+
     artiq-comtools = {
       url = "github:m-labs/artiq-comtools";
       inputs.nixpkgs.follows = "nixpkgs";
@@ -42,6 +47,7 @@
     self,
     nixpkgs,
     rust-overlay,
+    naersk,
     sipyco,
     src-pythonparser,
     artiq-comtools,
@@ -72,7 +78,7 @@
       extensions = ["rust-src"];
       targets = [];
     };
-    rustPlatform = pkgs.makeRustPlatform {
+    naerskLib = pkgs.callPackage naersk {
       rustc = rust;
       cargo = rust;
     };
@@ -279,16 +285,11 @@
       buildCommand ? "python -m artiq.gateware.targets.${target} -V ${variant}",
       experimentalFeatures ? [],
     }:
-      pkgs.stdenv.mkDerivation {
+      naerskLib.buildPackage {
         name = "artiq-board-${target}-${variant}";
-        phases = ["buildPhase" "checkPhase" "installPhase"];
-        cargoDeps = rustPlatform.importCargoLock {
-          lockFile = ./artiq/firmware/Cargo.lock;
-          outputHashes = {
-            "fringe-1.2.1" = "sha256-u7NyZBzGrMii79V+Xs4Dx9tCpiby6p8IumkUl7oGBm0=";
-            "tar-no-std-0.1.8" = "sha256-xm17108v4smXOqxdLvHl9CxTCJslmeogjm4Y87IXFuM=";
-          };
-        };
+        src = ./artiq/firmware;
+        additionalCargoLock = "${rust}/lib/rustlib/src/rust/Cargo.lock";
+        singleStep = true;
         nativeBuildInputs = [
           (pkgs.python3.withPackages (ps: [migen misoc (artiq.withExperimentalFeatures experimentalFeatures) ps.packaging]))
           rust
@@ -296,42 +297,37 @@
           pkgs.llvm_15
           pkgs.lld_15
           vivado
-          rustPlatform.cargoSetupHook
         ];
-        buildPhase = ''
-          ARTIQ_PATH=`python -c "import artiq; print(artiq.__path__[0])"`
-          ln -s $ARTIQ_PATH/firmware/Cargo.lock .
-          cargoSetupPostUnpackHook
-          cargoSetupPostPatchHook
-          ${buildCommand}
-        '';
-        doCheck = true;
-        checkPhase = ''
-          # Search for PCREs in the Vivado output to check for errors
-          check_log() {
-            grep -Pe "$1" artiq_${target}/${variant}/gateware/vivado.log && exit 1 || true
-          }
-          check_log "\d+ constraint not met\."
-          check_log "Timing constraints are not met\."
-        '';
-        installPhase = ''
-          mkdir $out
-          cp artiq_${target}/${variant}/gateware/top.bit $out
-          if [ -e artiq_${target}/${variant}/software/bootloader/bootloader.bin ]
-          then cp artiq_${target}/${variant}/software/bootloader/bootloader.bin $out
-          fi
-          if [ -e artiq_${target}/${variant}/software/runtime ]
-          then cp artiq_${target}/${variant}/software/runtime/runtime.{elf,fbi} $out
-          else cp artiq_${target}/${variant}/software/satman/satman.{elf,fbi} $out
-          fi
+        overrideMain = _: {
+          buildPhase = buildCommand;
+          doCheck = true;
+          checkPhase = ''
+            # Search for PCREs in the Vivado output to check for errors
+            check_log() {
+              grep -Pe "$1" artiq_${target}/${variant}/gateware/vivado.log && exit 1 || true
+            }
+            check_log "\d+ constraint not met\."
+            check_log "Timing constraints are not met\."
+          '';
+          installPhase = ''
+            mkdir $out
+            cp artiq_${target}/${variant}/gateware/top.bit $out
+            if [ -e artiq_${target}/${variant}/software/bootloader/bootloader.bin ]
+            then cp artiq_${target}/${variant}/software/bootloader/bootloader.bin $out
+            fi
+            if [ -e artiq_${target}/${variant}/software/runtime ]
+            then cp artiq_${target}/${variant}/software/runtime/runtime.{elf,fbi} $out
+            else cp artiq_${target}/${variant}/software/satman/satman.{elf,fbi} $out
+            fi
 
-          mkdir $out/nix-support
-          for i in $out/*.*; do
-          echo file binary-dist $i >> $out/nix-support/hydra-build-products
-          done
-        '';
-        # don't mangle ELF files as they are not for NixOS
-        dontFixup = true;
+            mkdir $out/nix-support
+            for i in $out/*.*; do
+            echo file binary-dist $i >> $out/nix-support/hydra-build-products
+            done
+          '';
+          # don't mangle ELF files as they are not for NixOS
+          dontFixup = true;
+        };
       };
 
     openocd-bscanspi-f = pkgs: let
