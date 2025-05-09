0b8aefe182905e757ea4d8d44d9b1343bdf97562
linuswck <linuswck@m-labs.hk>
2025-05-09 17:52:24 +0800

    artiq_flash: rewrite erase & write related actions - `gateware`,  `bootloader`, `storage`, `firmware` actions are removed - `write` command is added - User now can specify the flash regions for `write` and `erase` cmds     in "CMD:REGION" format

--- flake.nix
+++ flake.nix
@@ -609,7 +609,7 @@
 
             artiq_rtiomap --device-db $ARTIQ_ROOT/device_db.py device_map.bin
             artiq_mkfs -s ip `python -c "import artiq.examples.kc705_nist_clock.device_db as ddb; print(ddb.core_addr)"`/24 -f device_map device_map.bin kc705_nist_clock.config
-            artiq_flash -t kc705 -H rpi-1 storage -f kc705_nist_clock.config
+            artiq_flash write=storage -t kc705 -H rpi-1 -f kc705_nist_clock.config
             artiq_flash -t kc705 -H rpi-1 -d ${packages.x86_64-linux.artiq-board-kc705-nist_clock}
             sleep 30
 
