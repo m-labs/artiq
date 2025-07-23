41020484479cfcab60b3a246f557b4ea87c019b0
Sebastien Bourdeauducq <sb@m-labs.hk>
2025-07-23 21:23:56 +0800

    flake: add new required fields for buildPythonPackage

--- flake.nix
+++ flake.nix
@@ -107,13 +107,14 @@
     qasync = pkgs.python3Packages.buildPythonPackage rec {
       pname = "qasync";
       version = "0.27.1";
-      format = "pyproject";
       src = pkgs.fetchFromGitHub {
         owner = "CabbageDevelopment";
         repo = "qasync";
         rev = "refs/tags/v${version}";
         sha256 = "sha256-oXzwilhJ1PhodQpOZjnV9gFuoDy/zXWva9LhhK3T00g=";
       };
+      pyproject = true;
+      build-system = [pkgs.python3Packages.setuptools];
       postPatch = ''
         rm qasync/_windows.py # Ignoring it is not taking effect and it will not be used on Linux
       '';
@@ -128,6 +129,8 @@
       pname = "artiq";
       version = artiqVersion;
       src = self;
+      pyproject = true;
+      build-system = [pkgs.python3Packages.setuptools];
 
       preBuild = ''
         export VERSIONEER_OVERRIDE=${version}
@@ -178,7 +181,8 @@
     migen = pkgs.python3Packages.buildPythonPackage rec {
       name = "migen";
       src = src-migen;
-      format = "pyproject";
+      pyproject = true;
+      build-system = [pkgs.python3Packages.setuptools];
       nativeBuildInputs = [pkgs.python3Packages.setuptools];
       propagatedBuildInputs = [pkgs.python3Packages.colorama];
     };
@@ -192,12 +196,16 @@
         rev = version;
         sha256 = "sha256-ZHzgJnbsDVxVcp09LXq9JZp46+dorgdP8bAiTB59K28=";
       };
+      pyproject = true;
+      build-system = [pkgs.python3Packages.setuptools];
       propagatedBuildInputs = [pkgs.python3Packages.pyserial];
     };
 
     misoc = pkgs.python3Packages.buildPythonPackage {
       name = "misoc";
       src = src-misoc;
+      pyproject = true;
+      build-system = [pkgs.python3Packages.setuptools];
       propagatedBuildInputs = with pkgs.python3Packages; [jinja2 numpy migen pyserial asyncserial];
     };
 
@@ -210,6 +218,8 @@
         rev = "c21afe7a53258f05bde57e5ebf2e2761f3d495e4";
         sha256 = "sha256-jzyiLRuEf7p8LdhmZvOQj/dyQx8eUE8p6uRlwoiT8vg=";
       };
+      pyproject = true;
+      build-system = [pkgs.python3Packages.setuptools];
       propagatedBuildInputs = with pkgs.python3Packages; [pyserial prettytable msgpack migen];
     };
 
