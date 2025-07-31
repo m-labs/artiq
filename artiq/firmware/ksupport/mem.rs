736253fb2472858fd5f614fdd9a9beeb54929d29
htgazurex1212 <39023054+HTGAzureX1212@users.noreply.github.com>
2025-08-01 00:12:05 +0800

    remove refcout functions

--- artiq/firmware/ksupport/mem.rs
+++ artiq/firmware/ksupport/mem.rs
@@ -14,21 +14,7 @@ pub unsafe extern "C" fn nac3_malloc(size: usize) -> *mut u8 {
     rc_ptr.add(1).cast()
 }
 
-pub unsafe extern "C" fn nac3_rc_incr(ptr: *mut u8) {
-    let ptr = ptr.cast::<usize>().sub(1);
-    *ptr += 1;
-}
-
-pub unsafe extern "C" fn nac3_rc_decr(ptr: *mut u8) {
-    let rc_ptr = ptr.cast::<usize>().sub(1);
-    *rc_ptr -= 1;
-
-    if *ptr == 0 {
-        nac3_free(ptr);
-    }
-}
-
-pub unsafe fn nac3_free(ptr: *mut u8) {
+pub unsafe extern "C" fn nac3_free(ptr: *mut u8) {
     let size_ptr = ptr.sub(2 * mem::size_of::<usize>()).cast::<usize>();
 
     dealloc(
