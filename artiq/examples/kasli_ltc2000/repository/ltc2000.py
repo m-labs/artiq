65645cbd39d9e6f83013f096567de1fa2480206e
mwojcik <mw@m-labs.hk>
2025-08-08 15:18:31 +0800

    ltc2k: edit examples accordingly to driver and ddb template

--- artiq/examples/kasli_ltc2000/repository/ltc2000.py
+++ artiq/examples/kasli_ltc2000/repository/ltc2000.py
@@ -1,24 +1,22 @@
 from artiq.experiment import *
-from artiq.language.units import us, ms
 
-class DAC_Init(EnvExperiment):
-    MHz = 1e6
-
+class LtcExample(EnvExperiment):
     def build(self):
         self.setattr_device("core")
-        self.setattr_device("ltc_trigger")
-        self.setattr_device("ltc_dds0")
-        self.setattr_device("ltc_dds1")
-        self.setattr_device("ltc_clear")
+        self.setattr_device("ltc0_trigger")
+        self.setattr_device("ltc0_config")
+        self.setattr_device("ltc0_dds0")
+        self.setattr_device("ltc0_dds1")
+        self.setattr_device("ltc0_clear")
 
     @kernel
     def run(self):
         self.core.reset()
         self.core.break_realtime()
-        self.ltc_dds0.init()
-        self.ltc_clear.clear(1)
-        self.ltc_dds0.set_waveform(32768, 0, 0, 0, 0, 0x01000000, 0)
-        self.ltc_dds1.set_waveform(0, 0, 0, 0, 0, 0x02000000, 0)
-        self.ltc_trigger.trigger(0b1111)
+        self.ltc0_config.init()
+        self.ltc0_clear.clear(1)
+        self.ltc0_dds0.set_waveform(32768, 0, 0, 0, 0, 0x01000000, 0)
+        self.ltc0_dds1.set_waveform(0, 0, 0, 0, 0, 0x02000000, 0)
+        self.ltc0_trigger.trigger(0b1111)
         delay(1000*us)
-        self.ltc_clear.clear(0)
+        self.ltc0_clear.clear(0)
