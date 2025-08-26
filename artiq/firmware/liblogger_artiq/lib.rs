8863d7159a872c9c47cdf1d930c970aa059166d8
dly04 <yliujc@connect.ust.hk>
2025-08-26 15:20:53 +0800

    config log_level: store key & change levels at runtime reover json merge master
    can use without error handle

--- artiq/firmware/liblogger_artiq/lib.rs
+++ artiq/firmware/liblogger_artiq/lib.rs
@@ -11,6 +11,17 @@ use log::{Log, LevelFilter};
 use log_buffer::LogBuffer;
 use board_misoc::clock;
 
+pub enum Error{
+    Utf8Error(core::str::Utf8Error),
+    ParseError,
+}
+
+impl From<core::str::Utf8Error> for Error {
+    fn from(error: core::str::Utf8Error) -> Self {
+        Error::Utf8Error(error)
+    }
+}
+
 pub struct LogBufferRef<'a> {
     buffer:        RefMut<'a, LogBuffer<&'static mut [u8]>>,
     old_log_level: LevelFilter
@@ -100,6 +111,26 @@ impl BufferLogger {
         self.buffer_filter.set(max_level);
         self.update_global_log_level()
     }
+
+    // pub fn set_log_filter_level(&self, key: &str, value: &[u8]) -> Result<(), Error> {
+    //     let value_str = core::str::from_utf8(value)?;
+    //     let max_level = value_str.parse::<LevelFilter>()
+    //         .map_err(|_| Error::ParseError)?;
+
+    //     match key{
+    //         "log_level" => {
+    //             self.set_buffer_log_level(max_level);
+    //             log::info!("changing log level to {}", max_level);
+    //         }
+    //         "uart_log_level" => {
+    //             self.set_uart_log_level(max_level);
+    //             log::info!("changing UART log level to {}", max_level);
+    //         }
+    //         _ => ()
+    //     }
+
+    //     Ok(())
+    // }
 
     pub fn update_global_log_level(&self){
         let uart_level = self.uart_filter.get();
