4573088eed68d1b52752056c10e40ba40391823c
mwojcik <mw@m-labs.hk>
2025-08-28 10:28:29 +0800

    docs: add songbird driver reference

--- artiq/examples/kasli_songbird/repository/songbird.py
+++ artiq/examples/kasli_songbird/repository/songbird.py
@@ -1,22 +1,49 @@
 from artiq.experiment import *
+from artiq.coredevice.songbird import volt_to_mu
 
-class LtcExample(EnvExperiment):
+class SongbirdExample(EnvExperiment):
     def build(self):
         self.setattr_device("core")
-        self.setattr_device("songbird0_trigger")
         self.setattr_device("songbird0_config")
+        self.setattr_device("songbird0_trigger")
+        self.setattr_device("songbird0_clear")
         self.setattr_device("songbird0_dds0")
         self.setattr_device("songbird0_dds1")
-        self.setattr_device("songbird0_clear")
 
     @kernel
     def run(self):
         self.core.reset()
         self.core.break_realtime()
+
+        # Remember, LTC2000 requires a 2.4 or 2.5GHz sampling clock.
+        # Provide one before starting the example.
+        # Clear all DDS channels while configuring them.
+        self.songbird0_clear.clear(0b1111)
+        # Initialize the Songbird DAC with default settings.
         self.songbird0_config.init()
-        self.songbird0_clear.clear(1)
-        self.songbird0_dds0.set_waveform(32768, 0, 0, 0, 0, 0x01000000, 0)
-        self.songbird0_dds1.set_waveform(0, 0, 0, 0, 0, 0x02000000, 0)
-        self.songbird0_trigger.trigger(0b1111)
-        delay(1000*us)
+        delay(1*ms)
+        
+        # Set two waveforms, 50 and 100MHz
+        self.songbird0_dds0.set_waveform(
+            ampl_offset=volt_to_mu(0.5), 
+            damp=0, ddamp=0, dddamp=0,
+            phase_offset=0, 
+            ftw=self.songbird0_dds0.frequency_to_mu(50.0*MHz),
+            chirp=0,
+            shift=0
+        )
+
+        self.songbird0_dds1.set_waveform(
+            ampl_offset=volt_to_mu(0.25),
+            damp=0, ddamp=0, dddamp=0,
+            phase_offset=0,
+            ftw=self.songbird0_dds1.frequency_to_mu(100*MHz),
+            chirp=0,
+            shift=0
+        )
+        delay(10*us)
+
+        # Trigger channels 0 and 1
+        self.songbird0_trigger.trigger(0b0011)
+        delay(1*us)
         self.songbird0_clear.clear(0)
