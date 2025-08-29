8f0835d6f31f308701aac001d1aa1a79de6cdaa9
Robert Jördens <rj@quartiq.de>
2025-08-29 09:41:10 +0200

    phaser: consice IIR offset bounds check

--- artiq/coredevice/phaser.py
+++ artiq/coredevice/phaser.py
@@ -1270,7 +1270,7 @@ class PhaserChannel:
         forward_gain = (b0 + b1) * (1 << SERVO_DATA_WIDTH - 1 - SERVO_COEFF_SHIFT)
         effective_offset = int(round(DATA_MAX * y_offset + forward_gain * x_offset))
 
-        if (effective_offset >= DATA_MAX or effective_offset < -DATA_MAX):
+        if not -DATA_MAX <= effective_offset < DATA_MAX:
             raise ValueError("effective offset out of bounds")
 
         self.set_iir_mu(profile, b0, b1, a1, effective_offset)
