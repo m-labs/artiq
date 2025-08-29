c6cc78b1e07e04425f35534e563a71cebbd46dff
occheung <dc@m-labs.hk>
2025-08-29 16:35:37 +0800

    test: update channels parameter

--- artiq/gateware/test/suservo/test_iir.py
+++ artiq/gateware/test/suservo/test_iir.py
@@ -8,10 +8,12 @@ from artiq.gateware.suservo import iir
 def main():
     w_kasli = iir.IIRWidths(state=25, coeff=18, adc=16,
             asf=14, word=16, accu=48, shift=11,
-            channel=3, profile=5, dly=8)
+            adc_channels=8, dds_channels=4,
+            profile=5, dly=8)
     w = iir.IIRWidths(state=17, coeff=16, adc=16,
             asf=14, word=16, accu=48, shift=11,
-            channel=2, profile=1, dly=8)
+            adc_channels=4, dds_channels=4,
+            profile=1, dly=8)
 
     def run(dut):
         for i, ch in enumerate(dut.adc):
@@ -20,9 +22,10 @@ def main():
             yield ch.en_iir.eq(1)
             yield ch.en_out.eq(1)
             yield ch.profile.eq(i)
-        for i in range(1 << w.channel):
+        for i in range(w.adc_channels):
             yield from dut.set_state(i, i << 8, coeff="x1")
             yield from dut.set_state(i, i << 8, coeff="x0")
+        for i in range(w.dds_channels):
             for j in range(1 << w.profile):
                 yield from dut.set_state(i,
                         (j << 1) | (i << 8), profile=j, coeff="y1")
@@ -30,7 +33,7 @@ def main():
                     yield from dut.set_coeff(i, profile=j, coeff=l,
                             value=(i << 12) | (j << 8) | (k << 4))
         yield
-        for i in range(1 << w.channel):
+        for i in range(w.dds_channels):
             for j in range(1 << w.profile):
                 for k, l in enumerate("cfg a1 b0 b1".split()):
                     yield from dut.set_coeff(i, profile=j, coeff=l,
