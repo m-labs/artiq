m-labs/artiq

commit 13e9572d9ed19ac73fb3c9660307e749e13023c8

frontend: unify binary discovery process

artiq/frontend/artiq_flash.py
@@ -11,7 +11,7 @@ from sipyco import common_args
 
 from artiq import __version__ as artiq_version
 from artiq.remoting import SSHClient, LocalClient
-from artiq.frontend.flash_tools import artifact_path, bit2bin, fetch_bin
+from artiq.frontend.flash_tools import artifact_path, discover_bins
 
 
 def get_argparser():
@@ -348,13 +348,15 @@ def main():
 
     for cmd, regions in cmds:
         if cmd == "write":
+            found_bins = discover_bins(binary_dir, args.srcbuild)
             for region in regions:
-                if region == "firmware":
-                    path = fetch_bin(binary_dir, ["satman", "runtime"], args.srcbuild)
-                elif region == "storage":
+                if region == "storage":
                     path = args.storage
                 else:
-                    path = fetch_bin(binary_dir, [region], args.srcbuild)
+                    try:
+                        path = found_bins[region]
+                    except KeyError:
+                        raise FileNotFoundError(f"no binary found for {region}")
                 programmer.write_binary(*config[region], path)
         elif cmd == "load":
             gateware_bit = artifact_path(binary_dir, "gateware", "top.bit")

artiq/frontend/artiq_coremgmt.py
@@ -12,7 +12,7 @@ from artiq import __version__ as artiq_version
 from artiq.master.databases import DeviceDB
 from artiq.coredevice.comm_kernel import CommKernel
 from artiq.coredevice.comm_mgmt import CommMgmt
-from artiq.frontend.flash_tools import bit2bin, fetch_bin
+from artiq.frontend.flash_tools import bit2bin, discover_bins
 
 
 def get_argparser():
@@ -97,10 +97,10 @@ def get_argparser():
     t_flash = tools.add_parser("flash",
                                help="flash the core device and reboot into the new system")
 
-    p_directory = t_flash.add_argument("directory",
-                                       metavar="DIRECTORY", type=str,
-                                       help="directory that contains the "
-                                            "binaries")
+    p_path = t_flash.add_argument("path",
+                                  metavar="PATH", type=str,
+                                  help="binary file or directory that "
+                                       "contains the binaries")
 
     p_srcbuild = t_flash.add_argument("--srcbuild",
                                       help="board binaries directory is laid "
@@ -169,36 +169,16 @@ def main():
             mgmt.config_erase()
 
     if args.tool == "flash":
-        retrieved_bins = []
-        bin_dict = {
-            "zynq":[
-                ["boot"]
-            ],
-            "riscv": [
-                ["gateware"],
-                ["bootloader"],
-                ["runtime", "satman"],
-            ],
-        }
+        retrieved_bins = discover_bins(args.path, args.srcbuild)
 
-        for bin_list in bin_dict.values():
-            try:
-                bins = []
-                for bin_name in bin_list:
-                    bins.append(fetch_bin(
-                        args.directory, bin_name, args.srcbuild))
-                retrieved_bins.append(bins)
-            except FileNotFoundError:
-                pass
-
-        if retrieved_bins is None:
+        if len(retrieved_bins) == 0:
             raise FileNotFoundError("neither risc-v nor zynq binaries were found")
 
-        if len(retrieved_bins) > 1:
+        if "boot" in retrieved_bins.keys() and len(retrieved_bins) > 1:
             raise ValueError("both risc-v and zynq binaries were found, "
                              "please clean up your build directory. ")
 
-        bins = retrieved_bins[0]
+        bins = retrieved_bins.values()
         mgmt.flash(bins)
 
     if args.tool == "reboot":

artiq/frontend/flash_tools.py
@@ -4,6 +4,31 @@ import tempfile
 import struct
 
 
+def discover_bins(path, srcbuild=False):
+    if not os.path.exists(path):
+        raise FileNotFoundError("path does not exist")
+
+    if os.path.isfile(path):
+        if os.path.basename(path) == "boot.bin":
+            return {"boot": path}
+        raise ValueError("file is not a valid binary")
+
+    retrieved_bins = {}
+    bin_dict = {
+        "boot": ["boot"],
+        "gateware": ["gateware"],
+        "bootloader": ["bootloader"],
+        "firmware": ["runtime", "satman"],
+    }
+
+    for name, components in bin_dict.items():
+        try:
+            retrieved_bins[name] = fetch_bin(path, components, srcbuild)
+        except FileNotFoundError:
+            pass
+    return retrieved_bins
+
+
 def artifact_path(this_binary_dir, *path_filename, srcbuild=False):
     if srcbuild:
         # source tree - use path elements to locate file

commit dd0a25cbe63da913c392fcc95887833ddde1c0d9

fix artiq_flash storage only write
caused by 13e9572d

artiq/frontend/artiq_flash.py
@@ -348,7 +348,8 @@ def main():
 
     for cmd, regions in cmds:
         if cmd == "write":
-            found_bins = discover_bins(binary_dir, args.srcbuild)
+            found_bins = (discover_bins(binary_dir, args.srcbuild)
+                          if binary_dir is not None else {})
             for region in regions:
                 if region == "storage":
                     path = args.storage